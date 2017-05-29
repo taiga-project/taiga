f78d5ec47e7508a4b0cc65ecf8324b2110da9d23
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2017-05-29 02:23:59 +0000

    updated

--- plot.sh
+++ plot.sh
@@ -2,10 +2,10 @@
 source ./parameters.sh
 runnumber=`cut -d ',' -f2 runnumber`  
 
-matlabscript="cd plotter,try, detPlot('$shotnumber_$time','$runnumber', "$energy", "$angle"), catch, exit(1), end, exit(0);"
+matlabscript="cd plotter, try, detPlot('"$shotnumber"_"$time"','$runnumber', "$energy", "$angle", '_all', 0.18), catch, exit(1), end, exit(0);"
 eval '$matlab -nodesktop -r "$matlabscript"'
 echo "matlab exit code: $?"
 
-matlabscript="cd plotter,try, detPlot('$shotnumber_$time','$runnumber', "$energy", "$angle", '_spx'), catch, exit(1), end, exit(0);"
+matlabscript="cd plotter, try, detPlot('"$shotnumber"_"$time"','$runnumber', "$energy", "$angle", '_spx', 0.18), catch, exit(1), end, exit(0);"
 eval '$matlab -nodesktop -r "$matlabscript"'
 echo "matlab exit code: $?"
