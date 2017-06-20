b5ca16fddcf2f81797f900516f1303c4d28deccb
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2017-06-20 16:13:57 +0000

    Derivate added

--- preproc/renate110_to_taiga.m
+++ preproc/renate110_to_taiga.m
@@ -44,16 +44,29 @@ function renate110_to_taiga(varargin)
 	
 	out.rad      = renate.rad.max-data(:,in.index.rad);
 	out.ionyeald = data(:,in.index.ionyeald);
+	v = 1:length(ionyeald);
+	d = 1e-6;
+	out.ionrate = (interp1(v,out.ionyeald,v+d/2)-interp1(v,out.ionyeald,v-d/2) )/d;
 	
 	dlmwrite([foldername, 'rad.dat']     , out.rad     , 'precision','%.16e','delimiter','\t');  
 	dlmwrite([foldername, 'ionyeald.dat'], out.ionyeald, 'precision','%.16e','delimiter','\t'); 
 	
 	figure
-	plot(out.rad,out.ionyeald,'r','linewidth',2)
+	plot(out.rad,1-out.ionyeald,'r','linewidth',2)
 	ylim([0 1])   
     title (['Ionisation yield from RENATE @ ', upper(in.tokamak), ' #', in.shotNumber, ' (', num2str(in.time),' s) angle: ',num2str(renate.angle),' degree'])
     xlabel('R [m]')
     ylabel('ionisation yield')
 	saveas(gcf,[out.plotfolder,'/', in.shotNumber,'_',num2str(in.time),'/renate_',num2str(renate.angle),'.pdf'])
+	
+	
+	
+	figure
+	plot(out.rad,out.ionrate,'r','linewidth',2)
+	ylim([0 1])   
+    title (['Ion population from RENATE @ ', upper(in.tokamak), ' #', in.shotNumber, ' (', num2str(in.time),' s) angle: ',num2str(renate.angle),' degree'])
+    xlabel('R [m]')
+    ylabel('ion population')
+	saveas(gcf,[out.plotfolder,'/', in.shotNumber,'_',num2str(in.time),'/renate_',num2str(renate.angle),'_pop.pdf'])
 
 end
