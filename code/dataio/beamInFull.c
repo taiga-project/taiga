58403c024847705ef983ad46365a059b897fb525
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-01-15 20:45:49 +0000

    manual profile added

--- code/dataio/beamInFull.c
+++ code/dataio/beamInFull.c
@@ -13,13 +13,13 @@ mass:		in AMU
 // set beam inline parameters
 int beamIn(double *XR, double *XZ, double *XT, double *VR, double *VZ, double *VT, double energy, double eperm, int beam_number, char *shotname, double diameter, double deflH_degree, double deflV_degree){
 
-	int nrv = vectorReader0(&XR, "dataio/data/rad.dat");
-	vectorReader0(&XZ, "dataio/data/z.dat");
-	vectorReader0(&XT, "dataio/data/tor.dat");
+	int nrv = vectorReader0(&XR, "input/manual_profile/rad.dat");
+	vectorReader0(&XZ, "input/manual_profile/z.dat");
+	vectorReader0(&XT, "input/manual_profile/tor.dat");
 
-	vectorReader0(&VR, "dataio/data/vrad.dat");
-	vectorReader0(&VZ, "dataio/data/vz.dat");
-	vectorReader0(&VT, "dataio/data/vtor.dat");
+	vectorReader0(&VR, "input/manual_profile/vrad.dat");
+	vectorReader0(&VZ, "input/manual_profile/vz.dat");
+	vectorReader0(&VT, "input/manual_profile/vtor.dat");
 
 	printf("SIZEOF: %d\n",nrv);
 	
