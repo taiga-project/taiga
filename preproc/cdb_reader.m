a5a1ae71b0752d4c97ef8d40be6d072aa656de43
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-03-09 12:31:41 +0000

    Electric field module added | bug fix

--- preproc/cdb_reader.m
+++ preproc/cdb_reader.m
@@ -104,8 +104,8 @@ function efit = makeElectricGrid (in, out, efit)
     electric_field_components = regexprep(electric_field_components,'/','./');
     electric_field_components = regexprep(electric_field_components,'^','.^');
 
-    r = out.r;
-    z = out.z;
+    r = efit.r;
+    z = efit.z;
     
     erad=zeros(size(efit.brad));
     ez=zeros(size(efit.brad));
