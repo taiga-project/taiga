64fc6bc6c0d22c7f790b66098a331ab7cba60aab
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-03-12 11:00:41 +0000

    RUN Debug added

---
+++ run_debug.sh
@@ -0,0 +1,11 @@
+##!/usr/bin/bash
+source ./parameters.sh
+
+# set runnumber
+oldnum=`cut -d ',' -f2 runnumber`  
+newnum=`expr $oldnum + 1`
+sed -i "s/$oldnum\$/$newnum/g" runnumber 
+
+#./taiga.exe $shotnumber"_"$time $particles $angle
+#            <------ 1 -------> <- 2 ->  <-- 3 ---> <- 4 -> <-5->  <-- 6 --> <--- 7 ---> <-- 8 -->  <--------- 9 --------> (<10> <11> <12>)
+./taiga.exe $shotnumber"_"$time $newnum $beammatter $energy $angle $diameter $detector_R $particles $electric_field_module 2000
