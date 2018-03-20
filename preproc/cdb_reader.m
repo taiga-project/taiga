05d7a99d96979feba6f520d0e8fdc4073825bb6a
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-03-20 19:08:03 +0000

    Test database defined | bug fix: matrix dim fix + polflux='test' added

--- preproc/cdb_reader.m
+++ preproc/cdb_reader.m
@@ -120,8 +120,9 @@ function efit = makeMagneticGrid (in, out, efit)
     e = regexprep(e,'\^','.\^');
       
     s = [50,50];
-    efit.r = linspace(0.5,1.5,s(1))'.*ones(s);
-    efit.z = linspace(-0.5,0.5,s(2)).*ones(s);
+    efit.r = linspace(0.5,1.5,s(1)).*ones(s);
+    efit.z = linspace(-0.5,0.5,s(2))'.*ones(s);
+    efit.polflux = 'test';
     r = efit.r;
     z = efit.z;  
     
