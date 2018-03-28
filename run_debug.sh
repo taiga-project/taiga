2ad676df152e5953f9f70d160079635aec4174f8
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-03-28 16:30:21 +0000

    parameters.sh saved in output

--- run_debug.sh
+++ run_debug.sh
@@ -6,6 +6,9 @@ oldnum=`cut -d ',' -f2 runnumber`
 newnum=`expr $oldnum + 1`
 sed -i "s/$oldnum\$/$newnum/g" runnumber 
 
+mkdir "results/"$shotnumber"_"$time"/"$newnum
+cp parameters.sh "results/"$shotnumber"_"$time"/"$newnum
+
 #./taiga.exe $shotnumber"_"$time $particles $angle
 #            <------ 1 -------> <- 2 ->  <-- 3 ---> <- 4 -> <-5->  <-- 6 --> <--- 7 ---> <-- 8 -->  <--------- 9 --------> (<10> <11> <12>)
 ./taiga.exe $shotnumber"_"$time $newnum $beammatter $energy $angle $diameter $detector_R $particles $electric_field_module 2000  1     1
