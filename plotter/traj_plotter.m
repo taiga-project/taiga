f88a123b8fd769d3fa74af93d5d4e395ea63088e
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-03-29 17:52:18 +0000

    Particle id added

--- plotter/traj_plotter.m
+++ plotter/traj_plotter.m
@@ -14,30 +14,36 @@ function traj_plotter(varargin)
         runnumber = varargin{2};
     else        
         runnumber = '0';
-    end
+    end   
+
     
     load([mainfolder,'/',shotnumber,'/',runnumber,'/t_rad.dat']);
     load([mainfolder,'/',shotnumber,'/',runnumber,'/t_z.dat']);
     load([mainfolder,'/',shotnumber,'/',runnumber,'/t_tor.dat']);
     
+    
+    if nargin >= 3
+        t_index = varargin{2};
+        t_rad = t_rad(:,t_index);
+        t_z   = t_z  (:,t_index);
+        t_tor = t_tor(:,t_index);
+    end
+       
     figure
     subplot(2,2,1)
     plot(t_tor,t_z)
     xlabel('tor')
-    ylabel('z')
-    
+    ylabel('z')    
     
     subplot(2,2,2)
     plot(t_rad,t_z)
     xlabel('R')
-    ylabel('z')
-    
+    ylabel('z')    
     
     subplot(2,2,3)
     plot(t_tor,t_rad)
     xlabel('tor')
-    ylabel('R')
-    
+    ylabel('R')    
     
     subplot(2,2,4)
     plot3(t_rad,t_z,t_tor)
