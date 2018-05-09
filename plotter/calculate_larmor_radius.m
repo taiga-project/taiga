21e3150a0bfa7f1b99cbf5f869127131356ede79
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-05-09 18:16:25 +0000

    interp added | fix

--- plotter/calculate_larmor_radius.m
+++ plotter/calculate_larmor_radius.m
@@ -5,18 +5,18 @@
     
     load([mainfolder,'/',shotnumber,'/',runnumber,'/t_rad.dat']);
     load([mainfolder,'/',shotnumber,'/',runnumber,'/t_z.dat']);
-  %  load([mainfolder,'/',shotnumber,'/',runnumber,'/t_tor.dat']);
+    load([mainfolder,'/',shotnumber,'/',runnumber,'/t_tor.dat']);
     
     t_rad = t_rad';
     t_z = t_z';
     t_tor = t_tor';
     
-    L1=t_rad(2:end,:)<t_rad(1,:);
-    L2=t_rad(1:end-1,:)<t_rad(1,:);
+    L1=t_rad(:,2:end)<t_rad(:,1);
+    L2=t_rad(:,1:end-1)<t_rad(:,1);
     [I1,I2] = find(L1.*L2);
     I = find(L1.*L2);
     
-    zi = interp1([t_rad(I) t_rad(I+1)],[t_z(I) t_z(I+1)],t_rad(1,I2));
+    zi = interp1([t_rad(I) t_rad(I+1)],[t_z(I) t_z(I+1)],t_rad(I1,1));
     
     
     
