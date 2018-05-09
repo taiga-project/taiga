381e3a875c25fb6ddc37feeb98486074eb8f73f5
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-05-09 17:39:59 +0000

    index fix

--- plotter/calculate_larmor_radius.m
+++ plotter/calculate_larmor_radius.m
@@ -7,8 +7,8 @@
     load([mainfolder,'/',shotnumber,'/',runnumber,'/t_z.dat']);
     load([mainfolder,'/',shotnumber,'/',runnumber,'/t_tor.dat']);
     
-    L1=t_rad(:,2:end)<t_rad(:,1);
-    L2=t_rad(:,1:end-1)<t_rad(:,1);
+    L1=t_rad(2:end,:)<t_rad(1,:);
+    L2=t_rad(1:end-1,:)<t_rad(1,:);
     I = find(L1.*L2);
     t_rad(I)
     
