138a93f519778c022d8554b3fb965495fe28412f
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-05-09 13:18:11 +0000

    Calculate Larmor radius added

--- plotter/calculate_larmor_radius.m
+++ plotter/calculate_larmor_radius.m
@@ -8,9 +8,9 @@
     load([mainfolder,'/',shotnumber,'/',runnumber,'/t_tor.dat']);
     
     t_rad(1,:)
-    L1=t_rad(2:end,:)<t_rad(1,:)
-    L2=t_rad(1:end-1,:)<t_rad(1,:)
-    L1.*L2
+    L1=t_rad(:,2:end)<t_rad(:,1);
+    L2=t_rad(:,1:end-1)<t_rad(:,1);
+    find(L1.*L2)
     
     
     
