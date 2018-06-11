2e4930a97ee24d7c436b35e644d81ac222109960
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-06-11 18:24:23 +0000

    Multirun with angled detector

--- multi.sh
+++ multi.sh
@@ -2,6 +2,10 @@
 source ./parameters.sh
 runnumber=`cut -d ',' -f2 runnumber`  
 
-matlabscript="cd plotter, multirun_angled_det('"$shotnumber"_"$time"')"
+matlabscript="cd plotter, try, multirun_angled_det('"$shotnumber"_"$time"'), catch, exit(1), end, exit(0);"
+eval '$matlab -nodesktop -r "$matlabscript"'
+echo "matlab exit code: $?"
+
+matlabscript="cd plotter, try, detPlot('"$shotnumber"_"$time"','all', "$energy", "$angle", '_all', 0.22), catch, exit(1), end, exit(0);"
 eval '$matlab -nodesktop -r "$matlabscript"'
 echo "matlab exit code: $?"
