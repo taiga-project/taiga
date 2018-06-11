b7f8ed3fdfd91592e392125d2731a4315b26e152
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-06-11 15:53:56 +0000

    Multirun with angled detector

--- plotter/multirun_angled_det.m
+++ plotter/multirun_angled_det.m
@@ -1,4 +1,16 @@
 function multirun_angled_det(r)
-
+    d = dir(['../results/',r]);
+    
+    det_mid = [0.8 0.2 0];
+    det_ang = [1 0]; %z/R tor/R   
+    
+    
+    for i = 3:length(d)
+        t_rad = load[d(i).name,'/t_rad.dat']);
+        t_z = load[d(i).name,'/t_z.dat']);
+        t_tor = load[d(i).name,'/t_tor.dat']);
+        
+    end
+    keyboard    
 
 end
