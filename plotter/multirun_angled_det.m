5d2df2865e70cc5fb4fd7feb4357accc1ed1dd90
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-06-11 16:55:38 +0000

    Multirun with angled detector

--- plotter/multirun_angled_det.m
+++ plotter/multirun_angled_det.m
@@ -4,14 +4,37 @@ function multirun_angled_det(r)
     det_mid = [0.8 0.2 0];
     det_ang = [1 0]; %z/R tor/R   
     
+    rad = [];
+    z = [];
+    tor = [];
     
     for i = 3:length(d)
         if d(i).isdir
             t_rad = load([d(i).folder,'/',d(i).name,'/t_rad.dat']);
             t_z = load([d(i).folder,'/',d(i).name,'/t_z.dat']);
             t_tor = load([d(i).folder,'/',d(i).name,'/t_tor.dat']);
+        
+            t_plane = (t_rad-det_mid(1)) + (t_z-det_mid(2))*det_ang(1) + (t_tor-det_mid(3))*det_ang(2);
+        
+            ix = nan(1,size(t_rad,2));
+            for j = 1:size(t_rad,2)            
+                ix(j) = find(t_plane(:,j),1);
+            end
+            rad = mx_interp(t_rad,t_plane,ix);            
+            z   = mx_interp(t_z,  t_plane,ix);
+            tor = mx_interp(t_tor,t_plane,ix);
+            
         end
     end
     keyboard    
+
+end
+
+
+function I = mx_interp(A,B,ix);
+
+    s = size(A);
+    n = 1+ix*s(1);
+    I = A(n) - ( B(n) - B(n-1) ) ./ ( B(n)  ) .* ( A(n) - A(n-1) );
 
 end
