e2dd05ccf376d6e5744b7ef88ea25030dd547ea9
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-06-12 16:31:24 +0000

    Cross section distribution added | 1st try

--- code/dataio/beamInRenate110.c
+++ code/dataio/beamInRenate110.c
@@ -16,13 +16,20 @@ double linear_interpolate(double *x_vector, int x_length, double *y_vector, int 
 int beamIn(double *XR, double *XZ, double *XT, double *VR, double *VZ, double *VT, double energy, double eperm, int beam_number, char *shotname, double diameter, double deflH_degree, double deflV_degree){
 	int i;
 
-	double *prof_r, *prof_d, Vabs, ionisation_yeald;
+	double *prof_r, *prof_d, *profx_r, *profx_d, Vabs, ionisation_yeald, xsec_rad, xsec_ang;
 
 	int prof_r_length = vectorReader(&prof_r, "input/ionProf", shotname, "rad.dat");
-	int prof_d_length = vectorReader(&prof_d, "input/ionProf", shotname, "ionyeald.dat");
+	int prof_d_length = vectorReader(&prof_d, "input/ionProf", shotname, "ionyeald.dat");    
+    
+	int profx_r_length = vectorReader(&profx_r, "input/ionProf", shotname, "xrad.dat");
+	int profx_d_length = vectorReader(&profx_d, "input/ionProf", shotname, "xionyeald.dat");
 	
 	if (prof_r_length != prof_d_length){
-		printf("ERROR: Length of PROF_R and RPOF_D are different!\n");
+		printf("ERROR: Length of PROF_R and PROF_D are different!\n");
+	}
+	
+    if (profx_r_length != profx_d_length){
+		printf("ERROR: Length of PROFX_R and PROFX_D are different!\n");
 	}
 
 	double diam = diameter / 1000.0;
@@ -33,18 +40,32 @@ int beamIn(double *XR, double *XZ, double *XT, double *VR, double *VZ, double *V
 	Vabs = sqrt(2*energy*1000*eperm);
 	
 	/* one-ion beam */ 
+    
+	if (profx_r_length > 0){
+		for (i(0; i<profx_r_length; i++){
+			profx_d[i] /= profx_r[i];
+		}
+	}
 
 	/* initialize random generator */
 	srand ( time(NULL) );
 	for (i=0;i<beam_number;++i){
 		/* set position of particles */
 		do{
-		    ionisation_yeald = (double)rand()/RAND_MAX;
-		    XR[i] = linear_interpolate(prof_d, prof_d_length, prof_r, prof_r_length, ionisation_yeald);
-	    }while (isnan(XR[i])||XR[i]<0);
+			ionisation_yeald = (double)rand()/RAND_MAX;
+			XR[i] = linear_interpolate(prof_d, prof_d_length, prof_r, prof_r_length, ionisation_yeald);
+		}while (isnan(XR[i])||XR[i]<0);
 		do{
-			XZ[i]=(double)(rand()-RAND_MAX/2)/RAND_MAX*diam;
-			XT[i]=(double)(rand()-RAND_MAX/2)/RAND_MAX*diam;
+			if (profx_r_length == 0){
+				XZ[i]=(double)(rand()-RAND_MAX/2)/RAND_MAX*diam;
+				XT[i]=(double)(rand()-RAND_MAX/2)/RAND_MAX*diam;
+			}else{
+				ionisation_yeald = (double)rand()/RAND_MAX;
+				xsec_ang = (double)rand()/RAND_MAX*2*pi;
+				xsec_rad = linear_interpolate(profx_d, profx_d_length, profx_r, profx_r_length, ionisation_yeald)*(diam/2);
+                XZ[i]= sin(xsec_ang) * xsec_rad;
+                XT[i]= cos(xsec_ang) * xsec_rad;
+			}
 		}while ((XZ[i]*XZ[i]+XT[i]*XT[i])>=(diam/2)*(diam/2));
 		
 		/* toroidal deflection */
@@ -62,6 +83,8 @@ int beamIn(double *XR, double *XZ, double *XT, double *VR, double *VZ, double *V
 	return beam_number;
 
 }
+
+
 
 double linear_interpolate(double *x_vector, int x_length, double *y_vector, int y_length, double x_value){
 
