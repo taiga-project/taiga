2e14d90ddda0a1ef34aebd98733ca5cd2ec28f34
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-07-13 09:52:52 +0000

    New detector parameters added (38 degree, position)

--- parameters.sh
+++ parameters.sh
@@ -1,12 +1,12 @@
 ##!/usr/bin/bash
-shotnumber='14405'
-time='1250'
+shotnumber='17178'
+time='1097'
 beammatter='Li'
-energy=60 #keV
+energy=80 #keV
 angle=0 #degree
 diameter=25 #mm
 particles=100000
-detector="0.7089 0.2215 0 1 0"
+detector='0.685,0.23,0,0.781285,0'
 electric_field_module=0
 #electric_field_value='erad=10*exp((-(R-0.56)/0.11)^2/2);ez=0;etor=0'
 #electric_field_value='erad=10;ez=0;etor=0'
