d112f531a5d5696a183dd9dcaa94e9b4bfc70826
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-08-22 17:45:21 +0000

    Code cleaning

--- plot.sh
+++ plot.sh
@@ -5,7 +5,3 @@ runnumber=`cut -d ',' -f2 runnumber`
 matlabscript="cd plotter, try, detPlot('"$shotnumber"_"$time"','$runnumber', "$energy", "$angle", '_all', 0.18), catch, exit(1), end, exit(0);"
 eval '$matlab -nodesktop -r "$matlabscript"'
 echo "matlab exit code: $?"
-
-matlabscript="cd plotter, try, detPlot('"$shotnumber"_"$time"','$runnumber', "$energy", "$angle", '_spx', 0.18), catch, exit(1), end, exit(0);"
-eval '$matlab -nodesktop -r "$matlabscript"'
-echo "matlab exit code: $?"
