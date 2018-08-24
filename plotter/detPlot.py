71d6a2640a394d40efe1f2f317f5685d0aa8de81
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-08-24 10:23:40 +0000

    Python detector plotter

--- plotter/detPlot.py
+++ plotter/detPlot.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as pl
 import scipy.stats as st
 
+max_distance = 1e-4
+results_folder = '../results/'+shotnumber+'_'+time+'/'+runnumber+'/'
+
 try:
 	shotnumber = sys.argv[1];
 except:
@@ -20,13 +23,39 @@ try:
 except:
 	print 'Runnumber is not given as proper input parameter. Default value (402) is used.'
 	runnumber = '402'
+	
+try:
+	detector_par = sys.argv[4];
+except:
+	print 'Detector parameters is not given as proper input parameter. Default value (0.685,0.22,0,38,0) is used.'
+	detector_par = '0.685,0.22,0,38,0'
 
-detector_RZ_angle = 38
-R0 = 0.685
-Z0 = 0.23
-max_distance = 1e-4
+detector_par_list = detector_par.split(',')
 
-results_folder = '../results/'+shotnumber+'_'+time+'/'+runnumber+'/'
+try:
+	R0 = float(detector_par_list[0]);
+except:
+	R0 = 0.685
+
+try:
+	Z0 = float(detector_par_list[1]);
+except:
+	Z0 = 0.23
+
+try:
+	T0 = float(detector_par_list[2]);
+except:
+	T0 = 0.0
+
+try:
+	detector_RZ_angle = float(detector_par_list[3]);
+except:
+	detector_RZ_angle = 38.0
+
+try:
+	detector_ZT_angle = float(detector_par_list[4]);
+except:
+	detector_ZT_angle = 0.0
 
 try:
 	R = np.loadtxt(results_folder+'rad.dat') #, delimiter="\t"
@@ -66,3 +95,18 @@ try:
 
 except:
 	print 'Invalid input folder: '+results_folder
+	
+xmin, xmax = -4e-2, 4e-2
+ymin, ymax = -4e-2, 4e-2
+
+#x = T[particle_on_detector]-T0
+#y = (Z[particle_on_detector]-Z0) / np.sin( (detector_RZ_angle/180.0)*np.pi )
+#nbins=250
+#fig = pl.figure()
+#ax = fig.gca()
+#ax.set_xlim(xmin, xmax)
+#ax.set_ylim(ymin, ymax)
+#ax.set_aspect('equal')
+#cfset = ax.hist2d(x, y, bins=nbins, range = [[xmin,xmax],[ymin,ymax]], cmap='afmhot')
+#pl.colorbar(cfset[3]),ax=ax)
+#pl.show()
