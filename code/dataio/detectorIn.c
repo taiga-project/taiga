b3eb2d4564fae87ff4998f041b5e1a1cb592bfd6
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-09-19 17:14:53 +0000

    First try of detector postprocessor

--- code/dataio/detectorIn.c
+++ code/dataio/detectorIn.c
@@ -11,7 +11,7 @@ void detector_module(double **x_ptr, double *detector, int *detcellid, char *det
 	int N_detector_geometry_x = vectorReader(&detector_geometry_x, "input/detector", detector_name, "detx", false);
 	int N_detector_geometry_y = vectorReader(&detector_geometry_y, "input/detector", detector_name, "dety", false);
 
-	if ( N_detector_geometry_x > 0) && ( N_detector_geometry_y > 0){
+	if (( N_detector_geometry_x > 0) && ( N_detector_geometry_y > 0)) {
 		printf("Detector postprocessor module: ON");
 		detector_postproc <<< max_blocks, shot_block_size  >>> (x_ptr, detector_geometry_x, N_detector_geometry_x, detector_geometry_y, N_detector_geometry_y, detector, detcellid);
 	}else{
