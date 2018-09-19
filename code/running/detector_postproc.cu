b7b0ab1fbbc3bbaed36e0f878a04c31c578c35ae
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-09-19 18:08:40 +0000

    Test & mm

--- code/running/detector_postproc.cu
+++ code/running/detector_postproc.cu
@@ -8,23 +8,23 @@ __global__ void detector_postproc(double **x_ptr, double *det_x, int N_det_x, do
 	if (detcellid[idx] == 0){
 		double x,y;
 		
-		x = x_ptr[2][idx] - det[2];                x*=100;
-		y = (x_ptr[1][idx] - det[1]) / det[3];     y*=100;    
+		x = x_ptr[2][idx] - det[2];                x*=1000;
+		y = (x_ptr[1][idx] - det[1]) / det[3];     y*=1000;    
 		
 		int x_cellid = -1, y_cellid = -1;
 		
-		//if ((x >= det_x[0]) && (x <= det_x[N_det_x-1]) && (y >= det_y[0]) && (y <= det_y[N_det_y-1])) {
-			/*for (int i=0; i<(N_det_x/2); i++) {
+		if ((x >= det_x[0]) && (x <= det_x[N_det_x-1]) && (y >= det_y[0]) && (y <= det_y[N_det_y-1])) {
+			for (int i=0; i<(N_det_x/2); i++) {
 				if ((x >= det_x[2*i]) && (x <= det_x[2*i+1]))	x_cellid = i;
-			}*/
-			/*for (int j=0; j<N_det_y/2; j++) {
+			}
+			for (int j=0; j<N_det_y/2; j++) {
 				if ((y >= det_y[2*j]) && (y <= det_y[2*j+1]))	y_cellid = j;
-			}*/
-		//}
+			}
+		}
 		
-		//if ((x_cellid >= 0) && (y_cellid >= 0)) {
-			detcellid[idx] = x*1000;//x_cellid*1000+y_cellid;//(x_cellid-1)*N_det_y/2 + y_cellid;
-		//}
+		if ((x_cellid >= 0) && (y_cellid >= 0)) {
+			detcellid[idx] = x_cellid*1000+y_cellid;//(x_cellid-1)*N_det_y/2 + y_cellid;
+		}
 	}
 	/*if(idx==0){
 		 service_var[0] = 42.24;
