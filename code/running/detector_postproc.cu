b1a7b8edd430e1b059ce0851cd90c19525e22ad5
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-09-19 17:34:23 +0000

    Test

--- code/running/detector_postproc.cu
+++ code/running/detector_postproc.cu
@@ -5,34 +5,29 @@ __global__ void detector_postproc(double **x_ptr, double *det_x, int N_det_x, do
 	// thread index
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	
+	if (detcellid[idx] == 0){
+		double x,y;
 	
-	double x,y;
-/*
-	double det_R = det[0];
-	double det_z = det[1];
-	double det_tan = det[3];*/
-
-	x = x_ptr[2][idx] - det[2];                x*=100;
-	y = (x_ptr[1][idx] - det[1]) / det[3];     y*=100;    
-
-	int x_cellid = -1, y_cellid = -1;
-
-	detcellid[idx] = 0;
-
-	if ((x >= det_x[0]) && (x <= det_x[N_det_x-1]) && (y >= det_y[0]) && (y <= det_y[N_det_y-1])) 
-    {detcellid[idx] = 666; //test
-		for (int i=0; i<N_det_x; i++) {
-			if ((x >= det_x[2*i]) && (x <= det_x[2*i+1]))	x_cellid = i;
+		x = x_ptr[2][idx] - det[2];                x*=100;
+		y = (x_ptr[1][idx] - det[1]) / det[3];     y*=100;    
+	
+		int x_cellid = -1, y_cellid = -1;
+	
+		detcellid[idx] = 666;
+		/*
+		if ((x >= det_x[0]) && (x <= det_x[N_det_x-1]) && (y >= det_y[0]) && (y <= det_y[N_det_y-1])) {
+			for (int i=0; i<N_det_x; i++) {
+				if ((x >= det_x[2*i]) && (x <= det_x[2*i+1]))	x_cellid = i;
+			}
+			for (int j=0; j<N_det_y; j++) {
+				if ((y >= det_y[2*j]) && (y <= det_y[2*j+1]))	y_cellid = j;
+			}
 		}
-		for (int j=0; j<N_det_y; j++) {
-			if ((y >= det_y[2*j]) && (y <= det_y[2*j+1]))	y_cellid = j;
-		}
+		
+		if ((x_cellid >= 0) && (y_cellid >= 0)) {
+			detcellid[idx] = (x_cellid-1)*N_det_y/2 + y_cellid;
+		}*/
 	}
-	
-	if ((x_cellid >= 0) && (y_cellid >= 0)) {
-		detcellid[idx] = (x_cellid-1)*N_det_y/2 + y_cellid;
-	}
-	
 	/*if(idx==0){
 		 service_var[0] = 42.24;
 	}*/
