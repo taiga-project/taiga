5b7e37a5921007a4b83b90ed2b81e7ca2f7b5a8c
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-09-20 11:57:21 +0000

    Detector geometry host to device added | fix

--- code/dataio/detectorIn.c
+++ code/dataio/detectorIn.c
@@ -11,7 +11,7 @@ void detector_module(double **x_ptr, double *detector, int *detcellid, char *det
 	int N_dgx = vectorReader(&DGX, "input/detector", detector_name, "detx", false);
 	int N_dgy = vectorReader(&DGY, "input/detector", detector_name, "dety", false);    
 
-	if (( N_dgx > 0) && ( N_dgy > 0) && (N_dgx == N_dgy) {
+	if (( N_dgx > 0) && ( N_dgy > 0) && (N_dgx == N_dgy)) {
 		printf("Detector postprocessor module: ON (%d x %d)", N_detector_geometry_x/2, N_detector_geometry_y/2);        
 		size_t dimDG = N_dgx * sizeof(double);
 		cudaMalloc((void **) &dgx,  dimDG);
