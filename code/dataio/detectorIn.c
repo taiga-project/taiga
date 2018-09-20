b11e323da25c3b8284f2ba8c55ceb98149099861
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2018-09-20 11:56:50 +0000

    Detector geometry host to device added

--- code/dataio/detectorIn.c
+++ code/dataio/detectorIn.c
@@ -5,15 +5,20 @@
 
 // set beam inline parameters
 void detector_module(double **x_ptr, double *detector, int *detcellid, char *detector_name, int max_blocks, int shot_block_size){
-	double *detector_geometry_x;
-	double *detector_geometry_y;
+	double *DGX, *dgx;
+	double *DGY, *dgy;
 
-	int N_detector_geometry_x = vectorReader(&detector_geometry_x, "input/detector", detector_name, "detx", false);
-	int N_detector_geometry_y = vectorReader(&detector_geometry_y, "input/detector", detector_name, "dety", false);
+	int N_dgx = vectorReader(&DGX, "input/detector", detector_name, "detx", false);
+	int N_dgy = vectorReader(&DGY, "input/detector", detector_name, "dety", false);    
 
-	if (( N_detector_geometry_x > 0) && ( N_detector_geometry_y > 0)) {
-		printf("Detector postprocessor module: ON (%d x %d)", N_detector_geometry_x/2, N_detector_geometry_y/2);
-		detector_postproc <<< max_blocks, shot_block_size  >>> (x_ptr, detector_geometry_x, N_detector_geometry_x, detector_geometry_y, N_detector_geometry_y, detector, detcellid);
+	if (( N_dgx > 0) && ( N_dgy > 0) && (N_dgx == N_dgy) {
+		printf("Detector postprocessor module: ON (%d x %d)", N_detector_geometry_x/2, N_detector_geometry_y/2);        
+		size_t dimDG = N_dgx * sizeof(double);
+		cudaMalloc((void **) &dgx,  dimDG);
+		cudaMalloc((void **) &dgy,  dimDG);
+		cudaMemcpy(dgx, DGX, dimDG, cudaMemcpyHostToDevice);
+		cudaMemcpy(dgy, DGY, dimDG, cudaMemcpyHostToDevice);        
+		detector_postproc <<< max_blocks, shot_block_size  >>> (x_ptr, dgx, N_dgx, dgy, N_dgy, detector, detcellid);
 	}else{
 		printf("Detector postprocessor module: OFF");
 	}
