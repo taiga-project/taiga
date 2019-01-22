e0a73954e81772dfd15e53d947578237e013f821
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2019-01-22 20:23:54 +0000

    Code cleaning: electric_field_module moved to parameter_reader

--- code/dataio/parameter_reader.c
+++ code/dataio/parameter_reader.c
@@ -16,7 +16,7 @@ void init_taiga_props(char* par_name, char* par_value, shot_prop *shot, beam_pro
     double par_value_lf;    sscanf(par_value, "%lf", &par_value_lf);
     int par_value_d;        sscanf(par_value, "%d", &par_value_lf);
         
-    if (!strcmp(par_name, "shotnumber"))                    strcpy(shot->shotnumber, clean_string(par_value));
+    if (!strcmp(par_name, "shotnumber"))                    estrcpy(shot->shotnumber, clean_string(par_value));
     else if (!strcmp(par_name, "time"))                     strcpy(shot->time, clean_string(par_value));
     else if (!strcmp(par_name, "runnumber"))                shot->runnumber = par_value_lf;
     else if (!strcmp(par_name, "matter"))                   strcpy(beam->matter, clean_string(par_value));
