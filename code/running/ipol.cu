7ad006e46df6823fda44c3b30a91a0d5b80f8678
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2019-01-22 19:11:44 +0000

    Code cleaning: variable renaming

--- code/running/ipol.cu
+++ code/running/ipol.cu
@@ -1,24 +1,22 @@
-__device__ int calculate_detection_position(double l_r, double l_z, double l_t, double l_or, double l_oz, double l_ot, double *det, double *l_x, double l_vr){
+__device__ int calculate_detection_position(double position_rad, double position_z, double position_tor, double position_rad_prev, double position_z_prev, double position_tor_prev, double *detector_geometry, double *l_x, double l_vr){
 
 	int finished = 0;
 	
-	double det_R = det[0];
-	double det_z = det[1];
-	double det_tan = det[3];
+	double detector_R = detector_geometry[0];
+	double detector_z = detector_geometry[1];
+	double detector_tan = detector_geometry[3];
 	
-	double det_dist, det_odist;
-	det_dist = (l_r-det_R)+det_tan*(l_z-det_z);
-	det_odist = (l_or-det_R)+det_tan*(l_oz-det_z);
+	double detector_distance, detector_distance_prev;
+	detector_distance = (position_rad-detector_R) + detector_tan*(position_z-detector_z);
+	detector_distance_prev = (position_rad_prev-detector_R) + detector_tan*(position_z_prev-detector_z);
 	
-	if((det_dist*det_odist<=0) && (l_vr>0)){
+	if((detector_distance*detector_distance_prev<=0) && (l_vr>0)){
 
-		double di_rate = (-det_odist)/(det_dist-det_odist);
-		l_x[0] = l_or + di_rate*(l_r-l_or);;
-		l_x[1] = l_oz + di_rate*(l_z-l_oz);
-		l_x[2] = l_ot + di_rate*(l_t-l_ot);
-
+		double di_rate = (-detector_distance_prev)/(detector_distance-detector_distance_prev);
+		l_x[0] = position_rad_prev + di_rate*(position_rad-position_rad_prev);;
+		l_x[1] = position_z_prev   + di_rate*(position_z-position_z_prev);
+		l_x[2] = position_tor_prev + di_rate*(position_tor-position_tor_prev);
 		finished = 1;
-	
 	}
 	
 	return finished;
--- code/running/traj.cu
+++ code/running/traj.cu
@@ -1,23 +1,23 @@
-__device__ /*double int*/ void copy_local_field(double *r_grid, int NR, double *z_grid, int NZ, double l_r, double l_z, int *rzci, double *lp_br, double *lp_bz, double *lp_bt,  double **br_ptr, double **bz_ptr, double **bt_ptr){
+__device__ /*double int*/ void copy_local_field(double *r_grid, int NR, double *z_grid, int NZ, double position_rad, double position_z, int *local_spline_indices, double *local_spline_brad, double *local_spline_bz, double *local_spline_btor,  double **br_ptr, double **bz_ptr, double **bt_ptr){
 	int rci, zci;
 	int i, i2;
 	
-	for(rci=0;(r_grid[rci+1]<l_r)&&(rci<NR-1);rci++){;}	
+	for(rci=0;(r_grid[rci+1]<position_rad)&&(rci<NR-1);rci++){;}
 	
-	for(zci=0;(z_grid[zci+1]<l_z)&&(zci<NR-1);zci++){;}	
+	for(zci=0;(z_grid[zci+1]<position_z)&&(zci<NR-1);zci++){;}
 	
 	// Particle leave out the cell
-	if ((rzci[0]!=rci)||(rzci[1]!=zci)){
-		rzci[0]=rci;
-		rzci[1]=zci;
+	if ((local_spline_indices[0]!=rci)||(local_spline_indices[1]!=zci)){
+		local_spline_indices[0]=rci;
+		local_spline_indices[1]=zci;
 	
 		for(i=0;i<16;i++){
 	
-			i2 = (rzci[0])*(NZ-1)+rzci[1];
+			i2 = (local_spline_indices[0])*(NZ-1)+local_spline_indices[1];
 		
-			lp_br[i]=br_ptr[i][i2];
-			lp_bz[i]=bz_ptr[i][i2];
-			lp_bt[i]=bt_ptr[i][i2];
+			local_spline_brad[i]=br_ptr[i][i2];
+			local_spline_bz[i]=bz_ptr[i][i2];
+			local_spline_btor[i]=bt_ptr[i][i2];
 		
 		}
 	}
@@ -25,30 +25,30 @@ __device__ /*double int*/ void copy_local_field(double *r_grid, int NR, double *
 	//return i2;	
 }
 
-__device__ /*double int*/ void copy_local_field(double *r_grid, int NR, double *z_grid, int NZ, double l_r, double l_z, int *rzci, double *lp_br, double *lp_bz, double *lp_bt,  double **br_ptr, double **bz_ptr, double **bt_ptr,
-										double *lp_er, double *lp_ez, double *lp_et,  double **er_ptr, double **ez_ptr, double **et_ptr){
+__device__ /*double int*/ void copy_local_field(double *r_grid, int NR, double *z_grid, int NZ, double position_rad, double position_z, int *local_spline_indices, double *local_spline_brad, double *local_spline_bz, double *local_spline_btor,  double **br_ptr, double **bz_ptr, double **bt_ptr,
+										double *local_spline_erad, double *local_spline_ez, double *local_spline_etor,  double **er_ptr, double **ez_ptr, double **et_ptr){
 	int rci, zci;
 	int i, i2;
 	
-	for(rci=0;(r_grid[rci+1]<l_r)&&(rci<NR-1);rci++){;}	
+	for(rci=0;(r_grid[rci+1]<position_rad)&&(rci<NR-1);rci++){;}
 	
-	for(zci=0;(z_grid[zci+1]<l_z)&&(zci<NR-1);zci++){;}	
+	for(zci=0;(z_grid[zci+1]<position_z)&&(zci<NR-1);zci++){;}
 	
 	// Particle leave out the cell
-	if ((rzci[0]!=rci)||(rzci[1]!=zci)){
-		rzci[0]=rci;
-		rzci[1]=zci;
+	if ((local_spline_indices[0]!=rci)||(local_spline_indices[1]!=zci)){
+		local_spline_indices[0]=rci;
+		local_spline_indices[1]=zci;
 	
 		for(i=0;i<16;i++){
 	
-			i2 = (rzci[0])*(NZ-1)+rzci[1];
+			i2 = (local_spline_indices[0])*(NZ-1)+local_spline_indices[1];
 		
-			lp_br[i]=br_ptr[i][i2];
-			lp_bz[i]=bz_ptr[i][i2];
-			lp_bt[i]=bt_ptr[i][i2];
-			lp_er[i]=er_ptr[i][i2];
-			lp_ez[i]=ez_ptr[i][i2];
-			lp_et[i]=et_ptr[i][i2];
+			local_spline_brad[i]=br_ptr[i][i2];
+			local_spline_bz[i]=bz_ptr[i][i2];
+			local_spline_btor[i]=bt_ptr[i][i2];
+			local_spline_erad[i]=er_ptr[i][i2];
+			local_spline_ez[i]=ez_ptr[i][i2];
+			local_spline_etor[i]=et_ptr[i][i2];
 		
 		}
 	}
@@ -56,7 +56,7 @@ __device__ /*double int*/ void copy_local_field(double *r_grid, int NR, double *
 	//return i2;	
 }
 
-__device__ double calculate_local_field(double *lp_b, double dr, double dz){
+__device__ double calculate_local_field(double *local_spline, double dr, double dz){
 
 	/* MATLAB CODE:
 	sample2(3) =c11(bs1,bs2)*dsx^3*dsy^3 + c12(bs1,bs2)*dsx^3*dsy^2 + c13(bs1,bs2)*dsx^3*dsy + c14(bs1,bs2)*dsx^3 + ...
@@ -64,109 +64,108 @@ __device__ double calculate_local_field(double *lp_b, double dr, double dz){
 				c31(bs1,bs2)*dsx  *dsy^3 + c32(bs1,bs2)*dsx  *dsy^2 + c33(bs1,bs2)*dsx  *dsy + c34(bs1,bs2)*dsx    + ...
 				c41(bs1,bs2)      *dsy^3 + c42(bs1,bs2)      *dsy^2 + c43(bs1,bs2)      *dsy + c44(bs1,bs2);*/
 
-	double blocal = 0.0, tmp[16] ;
+	double local_field = 0.0, tmp[16] ;
 	for(int i=0;i<4;i++){
 		for(int j=0;j<4;j++){
-			tmp[i*4+j] = lp_b[i*4+j]*pow(dr,3-i)*pow(dz,3-j);
+			tmp[i*4+j] = local_spline[i*4+j]*pow(dr,3-i)*pow(dz,3-j);
 		}
 	}   
     
 	for(int i=0;i<4;i++){
     	for(int j=0;j<4;j++){
-			blocal+=tmp[i*4+j];
+			local_field+=tmp[i*4+j];
 		}
 	}   
 
-	return blocal;
+	return local_field;
 }
 
 
-__device__ int traj(double *r_grid, int NR, double *z_grid, int NZ, double *l_x, double *l_v, double **br_ptr, double **bz_ptr, double **bt_ptr, double eperm, double *det, int N_step, int local_detcellid){
+__device__ int traj(double *r_grid, int NR, double *z_grid, int NZ, double *position, double *speed, double **br_ptr, double **bz_ptr, double **bt_ptr, double eperm, double *detector_geometry, int N_step, int local_detcellid){
 
 	// next grid
-	int rzci[2];
-	rzci[0]=-1;
-	rzci[1]=-1;
+	int local_spline_indices[2];
+	local_spline_indices[0]=-1;
+	local_spline_indices[1]=-1;
 		
-	double lp_br[16];
-	double lp_bz[16];
-	double lp_bt[16];	
+	double local_spline_brad[16];
+	double local_spline_bz[16];
+	double local_spline_btor[16];
 	
-	double l_br=0,l_bz,l_bt;
+	double local_brad=0,local_bz,local_btor;
 	double dr,dz;
-	double l_rT;
+	double position_rad_torus;
 	
-	double l_vr, l_vz, l_vt, l_vor, l_voz, l_vot;
-	double l_r,  l_z,   l_t, l_or,  l_oz,  l_ot;
+	double speed_rad, speed_z, speed_tor, speed_rad_prev, speed_z_prev, speed_tor_prev;
+	double position_rad,  position_z,   position_tor, position_rad_prev,  position_z_prev,  position_tor_prev;
 
 	double X[6];
 	
 	int finished = local_detcellid + 1;
 	
-	l_r  = l_x[0];
-	l_z  = l_x[1];
-	l_t  = l_x[2];
+	position_rad  = position[0];
+	position_z  = position[1];
+	position_tor  = position[2];
 	
-	l_vr = l_v[0];
-	l_vz = l_v[1];
-	l_vt = l_v[2];
+	speed_rad = speed[0];
+	speed_z = speed[1];
+	speed_tor = speed[2];
 	
 	int loopi;
 	for (loopi=0;(loopi<N_step && (!finished));loopi++){
 		// Get local magnetic field
 
-		l_rT = cyl2tor_coord(l_r, l_t);
-		copy_local_field(r_grid,NR,z_grid,NZ,l_rT,l_z,rzci,lp_br,lp_bz,lp_bt,br_ptr,bz_ptr,bt_ptr);
+		position_rad_torus = cyl2tor_coord(position_rad, position_tor);
+		copy_local_field(r_grid,NR,z_grid,NZ,position_rad_torus,position_z,local_spline_indices,local_spline_brad,local_spline_bz,local_spline_btor,br_ptr,bz_ptr,bt_ptr);
 		
-		//dr = l_r-r_grid[rzci[0]];
-		dr = l_rT-r_grid[rzci[0]];
-		dz = l_z-z_grid[rzci[1]];
+		dr = position_rad_torus-r_grid[local_spline_indices[0]];
+		dz = position_z-z_grid[local_spline_indices[1]];
 	
-		l_br =  calculate_local_field(lp_br,dr,dz);
-		l_bz =  calculate_local_field(lp_bz,dr,dz);
-		l_bt =  calculate_local_field(lp_bt,dr,dz);
+		local_brad =  calculate_local_field(local_spline_brad,dr,dz);
+		local_bz =  calculate_local_field(local_spline_bz,dr,dz);
+		local_btor =  calculate_local_field(local_spline_btor,dr,dz);
 
-		l_br = cyl2tor_rad(l_br, l_bt, l_r, l_t);
-		l_bt = cyl2tor_field(l_br, l_bt, l_r, l_t);
+		local_brad = cyl2tor_rad(local_brad, local_btor, position_rad, position_tor);
+		local_btor = cyl2tor_field(local_brad, local_btor, position_rad, position_tor);
 	
 		// archivate coordinates
-		l_or  = l_r;	l_oz  = l_z;	l_ot  = l_t;
-		l_vor = l_vr;	l_voz = l_vz;	l_vot = l_vt;
+		position_rad_prev  = position_rad;	position_z_prev  = position_z;	position_tor_prev  = position_tor;
+		speed_rad_prev = speed_rad;	speed_z_prev = speed_z;	speed_tor_prev = speed_tor;
 
 		// new coordinates
-		X[0] = l_r;
-		X[1] = l_z;
-		X[2] = l_t;
+		X[0] = position_rad;
+		X[1] = position_z;
+		X[2] = position_tor;
 
 		// new speed components
-		X[3] = l_vr;
-		X[4] = l_vz;
-		X[5] = l_vt;
+		X[3] = speed_rad;
+		X[4] = speed_z;
+		X[5] = speed_tor;
 	
-		solve_diffeq(X, l_br, l_bz, l_bt, eperm);
+		solve_diffeq(X, local_brad, local_bz, local_btor, eperm);
 		
 		// new coordinates
-		l_r = X[0];
-		l_z = X[1];
-		l_t = X[2];
+		position_rad = X[0];
+		position_z   = X[1];
+		position_tor = X[2];
 
 		// new speed components
-		l_vr = X[3];
-		l_vz = X[4];
-		l_vt = X[5];
+		speed_rad = X[3];
+		speed_z   = X[4];
+		speed_tor = X[5];
 	
 		// finished? (interpolation)
-		finished = calculate_detection_position(l_r, l_z, l_t, l_or, l_oz, l_ot, det, l_x, l_vr);
+		finished = calculate_detection_position(position_rad, position_z, position_tor, position_rad_prev, position_z_prev, position_tor_prev, detector_geometry, position, speed_rad);
 	}
 	
-	l_v[0] = l_vr;
-	l_v[1] = l_vz;
-	l_v[2] = l_vt;
+	speed[0] = speed_rad;
+	speed[1] = speed_z;
+	speed[2] = speed_tor;
 
 	if (!finished){
-		l_x[0] = l_r; //NAN;
-		l_x[1] = l_z;
-		l_x[2] = l_t;
+		position[0] = position_rad;
+		position[1] = position_z;
+		position[2] = position_tor;
 	}
 	if (finished){
 		local_detcellid = 0;
@@ -174,101 +173,101 @@ __device__ int traj(double *r_grid, int NR, double *z_grid, int NZ, double *l_x,
 	return local_detcellid;
 }
 
-__device__ int traj(double *r_grid, int NR, double *z_grid, int NZ, double *l_x, double *l_v, double **br_ptr, double **bz_ptr, double **bt_ptr, double **er_ptr, double **ez_ptr, double **et_ptr, double eperm, double *det, int N_step, int local_detcellid){
+__device__ int traj(double *r_grid, int NR, double *z_grid, int NZ, double *position, double *speed, double **br_ptr, double **bz_ptr, double **bt_ptr, double **er_ptr, double **ez_ptr, double **et_ptr, double eperm, double *detector_geometry, int N_step, int local_detcellid){
 
 	// next grid
-	int rzci[2];
-	rzci[0]=-1;
-	rzci[1]=-1;
+	int local_spline_indices[2];
+	local_spline_indices[0]=-1;
+	local_spline_indices[1]=-1;
     
-	double lp_br[16];
-	double lp_bz[16];
-	double lp_bt[16];
+	double local_spline_brad[16];
+	double local_spline_bz[16];
+	double local_spline_btor[16];
 
-	double lp_er[16];
-	double lp_ez[16];
-	double lp_et[16];
+	double local_spline_erad[16];
+	double local_spline_ez[16];
+	double local_spline_etor[16];
 	
-	double l_br=0,l_bz=0,l_bt=0;
-	double l_er=0,l_ez=0,l_et=0;
-	double dr,dz;
-	double l_rT;
+	double local_brad=0,local_bz=0,local_btor=0;
+	double local_erad=0,local_ez=0,local_etor=0;
+	double dr, dz;
+	double position_rad_torus;
 	
-	double l_vr, l_vz, l_vt, l_vor, l_voz, l_vot;
-	double l_r,  l_z,   l_t, l_or,  l_oz,  l_ot;
+	double speed_rad, speed_z, speed_tor, speed_rad_prev, speed_z_prev, speed_tor_prev;
+	double position_rad,  position_z,   position_tor, position_rad_prev,  position_z_prev,  position_tor_prev;
 
 	double X[6];
 	
 	int finished = local_detcellid + 1;
 
-	l_r  = l_x[0];
-	l_z  = l_x[1];
-	l_t  = l_x[2];
+	position_rad  = position[0];
+	position_z    = position[1];
+	position_tor  = position[2];
 	
-	l_vr = l_v[0];
-	l_vz = l_v[1];
-	l_vt = l_v[2];
+	speed_rad = speed[0];
+	speed_z   = speed[1];
+	speed_tor = speed[2];
 	
 	int loopi;
 	for (loopi=0;(loopi<N_step && (!finished));loopi++){
 		// Get local magnetic field
 
-		l_rT = cyl2tor_coord(l_r, l_t);
-		copy_local_field(r_grid,NR,z_grid,NZ,l_rT,l_z,rzci,lp_br,lp_bz,lp_bt,br_ptr,bz_ptr,bt_ptr,lp_er,lp_ez,lp_et,er_ptr,ez_ptr,et_ptr);
+		position_rad_torus = cyl2tor_coord(position_rad, position_tor);
+		copy_local_field(r_grid,NR,z_grid,NZ,position_rad_torus,position_z,local_spline_indices,local_spline_brad,local_spline_bz,local_spline_btor,br_ptr,bz_ptr,bt_ptr,local_spline_erad,local_spline_ez,local_spline_etor,er_ptr,ez_ptr,et_ptr);
 		
-		dr = l_rT-r_grid[rzci[0]];
-		dz = l_z-z_grid[rzci[1]];
+		dr = position_rad_torus-r_grid[local_spline_indices[0]];
+		dz = position_z-z_grid[local_spline_indices[1]];
 	
-		l_br =  calculate_local_field(lp_br,dr,dz);
-		l_bz =  calculate_local_field(lp_bz,dr,dz);
-		l_bt =  calculate_local_field(lp_bt,dr,dz);
-		l_br = cyl2tor_rad(l_br, l_bt, l_r, l_t);
-		l_bt = cyl2tor_field(l_br, l_bt, l_r, l_t);
+		local_brad =  calculate_local_field(local_spline_brad,dr,dz);
+		local_bz =  calculate_local_field(local_spline_bz,dr,dz);
+		local_btor =  calculate_local_field(local_spline_btor,dr,dz);
+		local_brad = cyl2tor_rad(local_brad, local_btor, position_rad, position_tor);
+		local_btor = cyl2tor_field(local_brad, local_btor, position_rad, position_tor);
 		
-		l_er =  calculate_local_field(lp_er,dr,dz);
-		l_ez =  calculate_local_field(lp_ez,dr,dz);
-		l_et =  calculate_local_field(lp_et,dr,dz);
-		l_er = cyl2tor_rad(l_er, l_et, l_r, l_t);
-		l_et = cyl2tor_field(l_er, l_et, l_r, l_t);
+		local_erad =  calculate_local_field(local_spline_erad,dr,dz);
+		local_ez =  calculate_local_field(local_spline_ez,dr,dz);
+		local_etor =  calculate_local_field(local_spline_etor,dr,dz);
+		local_erad = cyl2tor_rad(local_erad, local_etor, position_rad, position_tor);
+		local_etor = cyl2tor_field(local_erad, local_etor, position_rad, position_tor);
 
 		// archivate coordinates
-		l_or  = l_r;	l_oz  = l_z;	l_ot  = l_t;
-		l_vor = l_vr;	l_voz = l_vz;	l_vot = l_vt;
+		position_rad_prev  = position_rad;	position_z_prev  = position_z;	position_tor_prev  = position_tor;
+		speed_rad_prev = speed_rad;	speed_z_prev = speed_z;	speed_tor_prev = speed_tor;
 
 		// new coordinates
-		X[0] = l_r;
-		X[1] = l_z;
-		X[2] = l_t;
+		X[0] = position_rad;
+		X[1] = position_z;
+		X[2] = position_tor;
 
 		// new speed components
-		X[3] = l_vr;
-		X[4] = l_vz;
-		X[5] = l_vt;
+		X[3] = speed_rad;
+		X[4] = speed_z;
+		X[5] = speed_tor;
 	
-		solve_diffeq(X, l_br, l_bz, l_bt, l_er, l_ez, l_et, eperm);
+		solve_diffeq(X, local_brad, local_bz, local_btor, local_erad, local_ez, local_etor, eperm);
 		
 		// new coordinates
-		l_r = X[0];
-		l_z = X[1];
-		l_t = X[2];
+		position_rad = X[0];
+		position_z   = X[1];
+		position_tor = X[2];
 
 		// new speed components
-		l_vr = X[3];
-		l_vz = X[4];
-		l_vt = X[5];
+		speed_rad = X[3];
+		speed_z   = X[4];
+		speed_tor = X[5];
 	
 		// finished? (interpolation)
-		finished = calculate_detection_position(l_r, l_z, l_t, l_or, l_oz, l_ot, det, l_x, l_vr);
+		finished = calculate_detection_position(position_rad, position_z, position_tor, position_rad_prev, position_z_prev, position_tor_prev, detector_geometry, position, speed_rad);
 	}
 	
-	l_v[0] = l_vr;
-	l_v[1] = l_vz;
-	l_v[2] = l_vt;
+	speed[0] = speed_rad;
+	speed[1] = speed_z;
+	speed[2] = speed_tor;
 	
 	if (!finished){
-		l_x[0] = l_r; //NAN;
-		l_x[1] = l_z;
-		l_x[2] = l_t;
+		position[0] = position_rad;
+		position[1] = position_z;
+		position[2] = position_tor;
 	}
 
 	if (finished){
