d60fde6c839afda8a4341d27e46588554f308402
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2019-01-22 18:39:14 +0000

    Code cleaning: Runge--Kutta variable order changed

--- code/running/rk4.cu
+++ code/running/rk4.cu
@@ -4,10 +4,10 @@
 Linearized operator of RK4
 
 -->
-	*X:		previous coordinates (input of rk4lin)
-	*S:		output of rk4op (RK4 component)
-	*Sp:		previous RK4 coefficient 
-	mSp:		multiplier for X argument
+	*X:		previous coordinates (input of solve_diffeq)
+	*S:		output of calculate_rk4_coeff (RK4 component)
+	*S_prev:		previous RK4 coefficient 
+	rk_weight:		multiplier for X argument
 	l_br:		radial component of local poloidal field
 	l_bz:		vertical component of local poloidal field
 	l_bt:		local toroidal field
@@ -15,70 +15,70 @@ Linearized operator of RK4
 	dt:		timestep
 
 <--
-	*S:		f(X  + mSp * Sp) * dt
+	*S:		f(X  + rk_weight * S_prev) * dt
 
 */
-__device__ void rk4op(double *X, double *S, double *Sp, double mSp, double l_br, double l_bz, double l_bt, double eperm){
+__device__ void calculate_rk4_coeff(double *X, double *S, double *S_prev, double rk_weight, double l_br, double l_bz, double l_bt, double eperm){
 
-	for (int i=0;i<6;i++){
-		S[i] = X[i] + mSp * Sp[i];
+	for (int i=3;i<6;i++){
+		S[i] = X[i] + rk_weight * S_prev[i];
 	}
 	
-	S[0] = S[1]; // f(R) = vR
-	S[2] = S[3]; // f(Z) = vZ
-	S[4] = S[5]; // f(T) = vT
+	S[0] = S[3]; // f(R) = vR
+	S[1] = S[4]; // f(Z) = vZ
+	S[2] = S[5]; // f(T) = vT
 
-	S[1] = eperm*(S[2]*l_bt - S[4]*l_bz);		// q/m * (X4*B3-X6*B2)  = q/m * (v2*B3-v3*B2)
-	S[3] = eperm*(S[4]*l_br - S[0]*l_bt);		// q/m * (X6*B1-X2*B3)  = q/m * (v3*B1-v1*B3)
-	S[5] = eperm*(S[0]*l_bz - S[2]*l_br);		// q/m * (X2*B2-X4*B1)  = q/m * (v1*B2-v2*B1)
-
-	for (int i=0;i<6;i++){
-		S[i] *= dt;
-	}
-}
-
-__device__ void rk4op(double *X, double *S, double *Sp, double mSp, double l_br, double l_bz,  double l_bt, double l_er, double l_ez, double l_et, double eperm){
-
-	for (int i=0;i<6;i++){
-		S[i] = X[i] + mSp * Sp[i];
-	}
-	
-	S[0] = S[1]; // f(R) = vR
-	S[2] = S[3]; // f(Z) = vZ
-	S[4] = S[5]; // f(T) = vT
-	S[1] = eperm*(l_er + S[2]*l_bt - S[4]*l_bz);		// q/m * (E1+X4*B3-X6*B2)  = q/m * (E1+v2*B3-v3*B2)
-	S[3] = eperm*(l_ez + S[4]*l_br - S[0]*l_bt);		// q/m * (E2+X6*B1-X2*B3)  = q/m * (E2+v3*B1-v1*B3)
-	S[5] = eperm*(l_et + S[0]*l_bz - S[2]*l_br);		// q/m * (E3+X2*B2-X4*B1)  = q/m * (E3+v1*B2-v2*B1)
+	S[3] = eperm*(S[1]*l_bt - S[2]*l_bz); // f(vR) = q/m * (vZ*BT - vT*BZ)
+	S[4] = eperm*(S[2]*l_br - S[0]*l_bt); // f(vZ) = q/m * (vT*BR - vR*BT)
+	S[5] = eperm*(S[0]*l_bz - S[1]*l_br); // f(vT) = q/m * (vR*BZ - vZ*BR)
 
 	for (int i=0;i<6;i++){
 		S[i] *= dt;
 	}
 }
 
-__device__ void rk4lin(double *X, double l_br, double l_bz, double l_bt, double eperm){
+__device__ void calculate_rk4_coeff(double *X, double *S, double *S_prev, double rk_weight, double l_br, double l_bz,  double l_bt, double l_er, double l_ez, double l_et, double eperm){
+
+	for (int i=3;i<6;i++){
+		S[i] = X[i] + rk_weight * S_prev[i];
+	}
 	
-	double S1[6],S2[6],S3[6],S4[6];
+	S[0] = S[3]; // f(R) = vR
+	S[1] = S[4]; // f(Z) = vZ
+	S[2] = S[5]; // f(T) = vT
+	S[3] = eperm*(l_er + S[1]*l_bt - S[2]*l_bz); // f(vR) = q/m * (ER + vZ*BT - vT*BZ)
+	S[4] = eperm*(l_ez + S[2]*l_br - S[0]*l_bt); // f(vZ) = q/m * (EZ + vT*BR - vR*BT)
+	S[5] = eperm*(l_et + S[0]*l_bz - S[1]*l_br); // f(vT) = q/m * (ET + vR*BZ - vZ*BR)
 
-	rk4op(X, S1, X,  0.0, l_br, l_bz, l_bt, eperm);
-	rk4op(X, S2, S1, 0.5, l_br, l_bz, l_bt, eperm);
-	rk4op(X, S3, S2, 0.5, l_br, l_bz, l_bt, eperm);
-	rk4op(X, S4, S3, 1.0, l_br, l_bz, l_bt, eperm);
-
-	for(int i=0;i<6;i++){
-		X[i] = X[i] + (S1[i] + 2*S2[i] + 2*S3[i] + S4[i])/6;		
+	for (int i=0;i<6;i++){
+		S[i] *= dt;
 	}
 }
 
-__device__ void rk4lin(double *X, double l_br, double l_bz, double l_bt, double l_er, double l_ez, double l_et, double eperm){
+__device__ void solve_diffeq(double *X, double l_br, double l_bz, double l_bt, double eperm){
 	
 	double S1[6],S2[6],S3[6],S4[6];
 
-	rk4op(X, S1, X,  0.0, l_br, l_bz, l_bt, l_er, l_ez, l_et, eperm);
-	rk4op(X, S2, S1, 0.5, l_br, l_bz, l_bt, l_er, l_ez, l_et, eperm);
-	rk4op(X, S3, S2, 0.5, l_br, l_bz, l_bt, l_er, l_ez, l_et, eperm);
-	rk4op(X, S4, S3, 1.0, l_br, l_bz, l_bt, l_er, l_ez, l_et, eperm);
+	calculate_rk4_coeff(X, S1, X,  0.0, l_br, l_bz, l_bt, eperm);
+	calculate_rk4_coeff(X, S2, S1, 0.5, l_br, l_bz, l_bt, eperm);
+	calculate_rk4_coeff(X, S3, S2, 0.5, l_br, l_bz, l_bt, eperm);
+	calculate_rk4_coeff(X, S4, S3, 1.0, l_br, l_bz, l_bt, eperm);
 
 	for(int i=0;i<6;i++){
-		X[i] = X[i] + (S1[i] + 2*S2[i] + 2*S3[i] + S4[i])/6;		
+		X[i] = X[i] + (S1[i] + 2*S2[i] + 2*S3[i] + S4[i])/6;
+	}
+}
+
+__device__ void solve_diffeq(double *X, double l_br, double l_bz, double l_bt, double l_er, double l_ez, double l_et, double eperm){
+	
+	double S1[6],S2[6],S3[6],S4[6];
+
+	calculate_rk4_coeff(X, S1, X,  0.0, l_br, l_bz, l_bt, l_er, l_ez, l_et, eperm);
+	calculate_rk4_coeff(X, S2, S1, 0.5, l_br, l_bz, l_bt, l_er, l_ez, l_et, eperm);
+	calculate_rk4_coeff(X, S3, S2, 0.5, l_br, l_bz, l_bt, l_er, l_ez, l_et, eperm);
+	calculate_rk4_coeff(X, S4, S3, 1.0, l_br, l_bz, l_bt, l_er, l_ez, l_et, eperm);
+
+	for(int i=0;i<6;i++){
+		X[i] = X[i] + (S1[i] + 2*S2[i] + 2*S3[i] + S4[i])/6;
 	}
 }
--- code/running/traj.cu
+++ code/running/traj.cu
@@ -1,4 +1,4 @@
-__device__ /*double int*/ void copyLocal(double *rg, int NR, double *zg, int NZ, double l_r, double l_z, int *rzci, double *lp_br, double *lp_bz, double *lp_bt,  double **br_ptr, double **bz_ptr, double **bt_ptr){
+__device__ /*double int*/ void copy_local_field(double *rg, int NR, double *zg, int NZ, double l_r, double l_z, int *rzci, double *lp_br, double *lp_bz, double *lp_bt,  double **br_ptr, double **bz_ptr, double **bt_ptr){
 	int rci, zci;
 	int i, i2;
 	
@@ -8,8 +8,8 @@ __device__ /*double int*/ void copyLocal(double *rg, int NR, double *zg, int NZ,
 	
 	// Particle leave out the cell
 	if ((rzci[0]!=rci)||(rzci[1]!=zci)){
-		rzci[0]=rci; 	
-		rzci[1]=zci; 	
+		rzci[0]=rci;
+		rzci[1]=zci;
 	
 		for(i=0;i<16;i++){
 	
@@ -25,7 +25,7 @@ __device__ /*double int*/ void copyLocal(double *rg, int NR, double *zg, int NZ,
 	//return i2;	
 }
 
-__device__ /*double int*/ void copyLocal(double *rg, int NR, double *zg, int NZ, double l_r, double l_z, int *rzci, double *lp_br, double *lp_bz, double *lp_bt,  double **br_ptr, double **bz_ptr, double **bt_ptr,
+__device__ /*double int*/ void copy_local_field(double *rg, int NR, double *zg, int NZ, double l_r, double l_z, int *rzci, double *lp_br, double *lp_bz, double *lp_bt,  double **br_ptr, double **bz_ptr, double **bt_ptr,
 										double *lp_er, double *lp_ez, double *lp_et,  double **er_ptr, double **ez_ptr, double **et_ptr){
 	int rci, zci;
 	int i, i2;
@@ -36,8 +36,8 @@ __device__ /*double int*/ void copyLocal(double *rg, int NR, double *zg, int NZ,
 	
 	// Particle leave out the cell
 	if ((rzci[0]!=rci)||(rzci[1]!=zci)){
-		rzci[0]=rci; 	
-		rzci[1]=zci; 	
+		rzci[0]=rci;
+		rzci[1]=zci;
 	
 		for(i=0;i<16;i++){
 	
@@ -56,7 +56,7 @@ __device__ /*double int*/ void copyLocal(double *rg, int NR, double *zg, int NZ,
 	//return i2;	
 }
 
-__device__ double localField(double *lp_b, double dr, double dz){
+__device__ double calculate_local_field(double *lp_b, double dr, double dz){
 
 	/* MATLAB CODE:
 	sample2(3) =c11(bs1,bs2)*dsx^3*dsy^3 + c12(bs1,bs2)*dsx^3*dsy^2 + c13(bs1,bs2)*dsx^3*dsy + c14(bs1,bs2)*dsx^3 + ...
@@ -67,7 +67,7 @@ __device__ double localField(double *lp_b, double dr, double dz){
 	double blocal = 0.0, tmp[16] ;
 	for(int i=0;i<4;i++){
 		for(int j=0;j<4;j++){
-			tmp[i*4+j] = lp_b[i*4+j]*pow(dr,3-i)*pow(dz,3-j);			
+			tmp[i*4+j] = lp_b[i*4+j]*pow(dr,3-i)*pow(dz,3-j);
 		}
 	}   
     
@@ -94,7 +94,6 @@ __device__ int traj(double *rg, int NR, double *zg, int NZ, double *l_x, double 
 	
 	double l_br=0,l_bz,l_bt;
 	double dr,dz;
-//	double drT;
 	double l_rT;
 	
 	double l_vr, l_vz, l_vt, l_vor, l_voz, l_vot;
@@ -120,15 +119,15 @@ __device__ int traj(double *rg, int NR, double *zg, int NZ, double *l_x, double 
 		// Get local magnetic field
 
 		l_rT = cyl2tor_coord(l_r, l_t);
-		copyLocal(rg,NR,zg,NZ,l_rT,l_z,rzci,lp_br,lp_bz,lp_bt,br_ptr,bz_ptr,bt_ptr);	
+		copy_local_field(rg,NR,zg,NZ,l_rT,l_z,rzci,lp_br,lp_bz,lp_bt,br_ptr,bz_ptr,bt_ptr);
 		
 		//dr = l_r-rg[rzci[0]];
 		dr = l_rT-rg[rzci[0]];
 		dz = l_z-zg[rzci[1]];
 	
-		l_br =  localField(lp_br,dr,dz);
-		l_bz =  localField(lp_bz,dr,dz);
-		l_bt =  localField(lp_bt,dr,dz);
+		l_br =  calculate_local_field(lp_br,dr,dz);
+		l_bz =  calculate_local_field(lp_bz,dr,dz);
+		l_bt =  calculate_local_field(lp_bt,dr,dz);
 
 		l_br = cyl2tor_rad(l_br, l_bt, l_r, l_t);
 		l_bt = cyl2tor_tor(l_br, l_bt, l_r, l_t);
@@ -139,24 +138,24 @@ __device__ int traj(double *rg, int NR, double *zg, int NZ, double *l_x, double 
 
 		// new coordinates
 		X[0] = l_r;
-		X[2] = l_z;
-		X[4] = l_t;
+		X[1] = l_z;
+		X[2] = l_t;
 
 		// new speed components
-		X[1] = l_vr;
-		X[3] = l_vz;
+		X[3] = l_vr;
+		X[4] = l_vz;
 		X[5] = l_vt;
 	
-		rk4lin(X, l_br, l_bz, l_bt, eperm);
+		solve_diffeq(X, l_br, l_bz, l_bt, eperm);
 		
 		// new coordinates
 		l_r = X[0];
-		l_z = X[2];
-		l_t = X[4];
+		l_z = X[1];
+		l_t = X[2];
 
 		// new speed components
-		l_vr = X[1];
-		l_vz = X[3];
+		l_vr = X[3];
+		l_vz = X[4];
 		l_vt = X[5];
 	
 		// finished? (interpolation)
@@ -184,14 +183,14 @@ __device__ int traj(double *rg, int NR, double *zg, int NZ, double *l_x, double 
 	int rzci[2];
 	rzci[0]=-1;
 	rzci[1]=-1;
-		
+    
 	double lp_br[16];
 	double lp_bz[16];
 	double lp_bt[16];
 
 	double lp_er[16];
 	double lp_ez[16];
-	double lp_et[16];	
+	double lp_et[16];
 	
 	double l_br=0,l_bz=0,l_bt=0;
 	double l_er=0,l_ez=0,l_et=0;
@@ -220,20 +219,20 @@ __device__ int traj(double *rg, int NR, double *zg, int NZ, double *l_x, double 
 		// Get local magnetic field
 
 		l_rT = cyl2tor_coord(l_r, l_t);
-		copyLocal(rg,NR,zg,NZ,l_rT,l_z,rzci,lp_br,lp_bz,lp_bt,br_ptr,bz_ptr,bt_ptr,lp_er,lp_ez,lp_et,er_ptr,ez_ptr,et_ptr);	
+		copy_local_field(rg,NR,zg,NZ,l_rT,l_z,rzci,lp_br,lp_bz,lp_bt,br_ptr,bz_ptr,bt_ptr,lp_er,lp_ez,lp_et,er_ptr,ez_ptr,et_ptr);
 		
 		dr = l_rT-rg[rzci[0]];
 		dz = l_z-zg[rzci[1]];
 	
-		l_br =  localField(lp_br,dr,dz);
-		l_bz =  localField(lp_bz,dr,dz);
-		l_bt =  localField(lp_bt,dr,dz);
+		l_br =  calculate_local_field(lp_br,dr,dz);
+		l_bz =  calculate_local_field(lp_bz,dr,dz);
+		l_bt =  calculate_local_field(lp_bt,dr,dz);
 		l_br = cyl2tor_rad(l_br, l_bt, l_r, l_t);
 		l_bt = cyl2tor_tor(l_br, l_bt, l_r, l_t);
 		
-		l_er =  localField(lp_er,dr,dz);
-		l_ez =  localField(lp_ez,dr,dz);
-		l_et =  localField(lp_et,dr,dz);		
+		l_er =  calculate_local_field(lp_er,dr,dz);
+		l_ez =  calculate_local_field(lp_ez,dr,dz);
+		l_et =  calculate_local_field(lp_et,dr,dz);		
 		l_er = cyl2tor_rad(l_er, l_et, l_r, l_t);
 		l_et = cyl2tor_tor(l_er, l_et, l_r, l_t);
 
@@ -243,24 +242,24 @@ __device__ int traj(double *rg, int NR, double *zg, int NZ, double *l_x, double 
 
 		// new coordinates
 		X[0] = l_r;
-		X[2] = l_z;
-		X[4] = l_t;
+		X[1] = l_z;
+		X[2] = l_t;
 
 		// new speed components
-		X[1] = l_vr;
-		X[3] = l_vz;
+		X[3] = l_vr;
+		X[4] = l_vz;
 		X[5] = l_vt;
 	
-		rk4lin(X, l_br, l_bz, l_bt, l_er, l_ez, l_et, eperm);
+		solve_diffeq(X, l_br, l_bz, l_bt, l_er, l_ez, l_et, eperm);
 		
 		// new coordinates
 		l_r = X[0];
-		l_z = X[2];
-		l_t = X[4];
+		l_z = X[1];
+		l_t = X[2];
 
 		// new speed components
-		l_vr = X[1];
-		l_vz = X[3];
+		l_vr = X[3];
+		l_vz = X[4];
 		l_vt = X[5];
 	
 		// finished? (interpolation)
