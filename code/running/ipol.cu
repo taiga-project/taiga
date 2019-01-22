6c0c49544ad95c3f0e78ec39529b2f17059f1b4e
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2019-01-22 19:22:34 +0000

    Code cleaning: variable renaming

--- code/running/ipol.cu
+++ code/running/ipol.cu
@@ -1,4 +1,4 @@
-__device__ int calculate_detection_position(double position_rad, double position_z, double position_tor, double position_rad_prev, double position_z_prev, double position_tor_prev, double *detector_geometry, double *l_x, double l_vr){
+__device__ int calculate_detection_position(double position_rad, double position_z, double position_tor, double position_rad_prev, double position_z_prev, double position_tor_prev, double *detector_geometry, double *position, double speed_rad){
 
 	int finished = 0;
 	
@@ -10,12 +10,12 @@ __device__ int calculate_detection_position(double position_rad, double position
 	detector_distance = (position_rad-detector_R) + detector_tan*(position_z-detector_z);
 	detector_distance_prev = (position_rad_prev-detector_R) + detector_tan*(position_z_prev-detector_z);
 	
-	if((detector_distance*detector_distance_prev<=0) && (l_vr>0)){
+	if((detector_distance*detector_distance_prev<=0) && (speed_rad>0)){
 
 		double di_rate = (-detector_distance_prev)/(detector_distance-detector_distance_prev);
-		l_x[0] = position_rad_prev + di_rate*(position_rad-position_rad_prev);;
-		l_x[1] = position_z_prev   + di_rate*(position_z-position_z_prev);
-		l_x[2] = position_tor_prev + di_rate*(position_tor-position_tor_prev);
+		position[0] = position_rad_prev + di_rate*(position_rad-position_rad_prev);;
+		position[1] = position_z_prev   + di_rate*(position_z-position_z_prev);
+		position[2] = position_tor_prev + di_rate*(position_tor-position_tor_prev);
 		finished = 1;
 	}
 	
