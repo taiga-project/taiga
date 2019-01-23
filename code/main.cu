1afad5b0f28a814c5a97d8d7d60d9df7dfd70c56
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2019-01-23 17:50:13 +0000

    Minor: Error message updated

--- code/main.cu
+++ code/main.cu
@@ -299,7 +299,7 @@ int main(int argc, char *argv[]){
 	//! MEMCOPY (device2HOST)
 	cudaMemcpy(SERVICE_VAR, service_var, dimService, cudaMemcpyDeviceToHost);
 	if(SERVICE_VAR[0]!=42.24){
-		printf("\n +--------------------------+\n | Fatal error in running.    | \n | The CUDA did not run well. |\n +---------------------------+\n");
+		printf("\n +----------------------------+\n | Fatal error in running.    | \n | The CUDA did not run well. |\n +----------------------------+\n");
 	}else{
 		printf("\n	Memcopy OK.\n");
 	}
