6d3375e42e05fb498860cb88db90fce5b0836abe
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2019-01-29 20:42:34 +0000

    Interpolation cleaned

--- code/running/ipol.cu
+++ code/running/ipol.cu
@@ -1,3 +1,11 @@
+__device__ double interpolate(double x1, double x2, double x, double y1, double y2, double x2_x1){ 
+    return (x2-x)/x2_x1*y1 + (x-x1)/x2_x1*y2;
+}
+
+__device__ double interpolate(double x1, double x2, double x, double y1, double y2){
+    return interpolate(x1, x2, y1, y2, x2-x1);
+}
+
 __device__ int calculate_detection_position(double position_rad, double position_z, double position_tor, double position_rad_prev, double position_z_prev, double position_tor_prev, double *detector_geometry, double *position, double speed_rad){
 
     int finished = 0;
@@ -12,10 +20,10 @@ __device__ int calculate_detection_position(double position_rad, double position
     
     if((detector_distance*detector_distance_prev<=0) && (speed_rad>0)){
 
-        double di_rate = (-detector_distance_prev)/(detector_distance-detector_distance_prev);
-        position[0] = position_rad_prev + di_rate*(position_rad-position_rad_prev);;
-        position[1] = position_z_prev   + di_rate*(position_z-position_z_prev);
-        position[2] = position_tor_prev + di_rate*(position_tor-position_tor_prev);
+        double detector_distance_change = (detector_distance-detector_distance_prev);
+        position[0] = interpolate(position_rad_prev, position_rad, detector_distance_prev, detector_distance, detector_distance_change);
+        position[1] = interpolate(position_z_prev,   position_z,   detector_distance_prev, detector_distance, detector_distance_change);
+        position[2] = interpolate(position_tor_prev, position_tor, detector_distance_prev, detector_distance, detector_distance_change);
         finished = 1;
     }
     
