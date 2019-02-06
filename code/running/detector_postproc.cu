ce937a08757e76b395d736d68ace9f6b0fe6f4b0
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2019-02-06 11:11:25 +0000

    Side effect fixed (x_ptr)

--- code/running/detector_postproc.cu
+++ code/running/detector_postproc.cu
@@ -1,38 +1,26 @@
-// Trajectory simulation
+__global__ void detector_postproc(double **x_ptr, double *det_x, int N_det_x, double *det_y, int N_det_y, double *det, int *detcellid){
 
-//__constant__ int finCounter = 0 ;
-__global__ void detector_postproc(double **x_ptr, double *det_x, int N_det_x, double *det_y, int N_det_y, double *det, int *detcellid/*, double *service_var*/){
-	// thread index
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	
-	if (detcellid[idx] == 0){
-		double x,y;
-		
-		x = x_ptr[2][idx] - det[2];                x*=1000;
-		y = (det[1] - x_ptr[1][idx]) / det[3];     y*=1000;    
-		
-		int x_cellid = -1, y_cellid = -1;
-		
-		if ((x >= det_x[0]) & (x <= det_x[N_det_x-1]) & (y >= det_y[0]) & (y <= det_y[N_det_y-1])) {
-			for (int i=0; i<(N_det_x/2); i++) {
-				if ((x >= det_x[2*i]) & (x <= det_x[2*i+1]))	x_cellid = i;
-			}
-			for (int j=0; j<N_det_y/2; j++) {
-				if ((y >= det_y[2*j]) & (y <= det_y[2*j+1]))	y_cellid = j;
-			}
-		}
-		
-		if ((x_cellid >= 0) & (y_cellid >= 0)) {
-			detcellid[idx] = y_cellid * N_det_x/2 + x_cellid + 1;
-		}
-        x_ptr[0][idx]=1;x_ptr[1][idx]=x;x_ptr[2][idx]=y;
-	}else{
-        x_ptr[0][idx]=0;x_ptr[1][idx]=0;x_ptr[2][idx]=0;
-    }
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
     
+    if (detcellid[idx] == 0){
+        double x,y;
         
-	/*if(idx==0){
-		 service_var[0] = 42.24;
-	}*/
+        x = x_ptr[2][idx] - det[2];                x*=1000; //in mm
+        y = (det[1] - x_ptr[1][idx]) / det[3];     y*=1000; //in mm
+        
+        int x_cellid = -1, y_cellid = -1;
+        
+        if ((x >= det_x[0]) & (x <= det_x[N_det_x-1]) & (y >= det_y[0]) & (y <= det_y[N_det_y-1])) {
+            for (int i=0; i<(N_det_x/2); i++) {
+                if ((x >= det_x[2*i]) & (x <= det_x[2*i+1]))    x_cellid = i;
+            }
+            for (int j=0; j<N_det_y/2; j++) {
+                if ((y >= det_y[2*j]) & (y <= det_y[2*j+1]))    y_cellid = j;
+            }
+        }
+        
+        if ((x_cellid >= 0) & (y_cellid >= 0)) {
+            detcellid[idx] = y_cellid * N_det_x/2 + x_cellid + 1;
+        }
+    }
 }
-//https://www.reverbnation.com/riky9550
