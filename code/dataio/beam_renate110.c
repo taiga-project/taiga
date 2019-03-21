55c96bfafc26bd846b56225fa99dcaa769c5e8b1
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2019-03-21 18:54:48 +0000

    separate load_ion_profile

--- code/dataio/beam_renate110.c
+++ code/dataio/beam_renate110.c
@@ -1,38 +1,21 @@
 #include <stdlib.h>
 #include <math.h>
+#include beam.h
 
 // set beam inline parameters
 void load_beam(double *XR, double *XZ, double *XT, double *VR, double *VZ, double *VT, beam_prop beam, shot_prop shot, run_prop run){
     int i;
-    double *prof_r, *prof_d, *profx_r, *profx_d, Vabs, ionisation_yeald, xsec_rad, xsec_ang;
+    double *prof_size, *prof_r, *prof_d, *profx_r, *profx_d, Vabs, ionisation_yeald, xsec_rad, xsec_ang;
     
     char* shotname = concat(shot.shotnumber, "_", shot.time);
 
-    int prof_r_length = read_vector(&prof_r, "input/ionProf", shotname, "rad.dat");
-    int prof_d_length = read_vector(&prof_d, "input/ionProf", shotname, "ionyeald.dat");    
-    
-    int profx_r_length = read_vector(&profx_r, "input/ionProf", shotname, "xrad.dat", false);
-    int profx_d_length = read_vector(&profx_d, "input/ionProf", shotname, "xionyeald.dat", false);
-    
-    if (prof_r_length != prof_d_length){
-        printf("ERROR: Length of PROF_R and PROF_D are different!\n");
-    }
-
-    if (profx_r_length != profx_d_length){
-        printf("ERROR: Length of PROFX_R and PROFX_D are different!\n");
-    }
-
-    if (profx_r_length <= 0){
-        printf("Cross section beam profile: OFF\n");
-    }else{
-        printf("Cross section beam profile: ON\n");
-    }
+    load_ion_profile(shotname, prof_size, prof_r, prof_d, profx_r, profx_d);
     
     Vabs = sqrt(2 * beam.energy*1000*ELEMENTARY_CHARGE/ beam.mass/ AMU);
     
     /* cross section normalisation */
-    if (profx_r_length > 0){
-        for (i=0; i<profx_r_length; i++){
+    if (prof_size[1] > 0){
+        for (i=0; i<prof_size[1]; i++){
             profx_d[i] /= profx_r[i];
         }
     }
@@ -44,16 +27,16 @@ void load_beam(double *XR, double *XZ, double *XT, double *VR, double *VZ, doubl
         /* set position of particles */
         do{
             ionisation_yeald = (double)rand()/RAND_MAX;
-            XR[i] = linear_interpolate(prof_d, prof_d_length, prof_r, prof_r_length, ionisation_yeald);
+            XR[i] = linear_interpolate(prof_d, prof_size[0], prof_r, prof_size[0], ionisation_yeald);
         }while (isnan(XR[i])||XR[i]<0);
         do{
-            if (profx_r_length <= 0){
+            if (prof_size[1] <= 0){
                 XZ[i]=(double)(rand()-RAND_MAX/2)/RAND_MAX*beam.diameter;
                 XT[i]=(double)(rand()-RAND_MAX/2)/RAND_MAX*beam.diameter;
             }else{
                 ionisation_yeald = (double)rand()/RAND_MAX;
                 xsec_ang = (double)rand()/RAND_MAX*2*PI;
-                xsec_rad = linear_interpolate(profx_d, profx_d_length, profx_r, profx_r_length, ionisation_yeald)*(beam.diameter/2);
+                xsec_rad = linear_interpolate(profx_d, prof_size[1], profx_r, prof_size[1], ionisation_yeald)*(beam.diameter/2);
                 XZ[i]= sin(xsec_ang) * xsec_rad;
                 XT[i]= cos(xsec_ang) * xsec_rad;
             }
@@ -67,4 +50,33 @@ void load_beam(double *XR, double *XZ, double *XT, double *VR, double *VZ, doubl
         VZ[i] =  Vabs*sin(beam.vertical_deflection);
         VT[i] =  Vabs*cos(beam.vertical_deflection)*sin(beam.toroidal_deflection);
     }
+}
+
+void load_ion_profile(char* shotname, double *prof_size, double *prof_r, double *prof_d, double *profx_r, double *profx_d){
+    int prof_r_length = read_vector(&prof_r, "input/ionProf", shotname, "rad.dat");
+    int prof_d_length = read_vector(&prof_d, "input/ionProf", shotname, "ionyeald.dat");    
+    
+    int profx_r_length = read_vector(&profx_r, "input/ionProf", shotname, "xrad.dat", false);
+    int profx_d_length = read_vector(&profx_d, "input/ionProf", shotname, "xionyeald.dat", false);
+    
+    if (prof_r_length == prof_d_length){
+        prof_size[0] = prof_r_length;
+    }else{
+        printf("ERROR: Length of PROF_R and PROF_D are different!\n");
+        prof_size[0] = -1;
+    }
+
+    if (profx_r_length == profx_d_length){
+        prof_size[1] = profx_r_length;
+    }else{
+        printf("ERROR: Length of PROFX_R and PROFX_D are different!\n");
+        prof_size[1] = -1;
+    }
+
+    if (prof_size[1] <= 0){
+        printf("Cross section beam profile: OFF\n");
+    }else{
+        printf("Cross section beam profile: ON\n");
+    }
+    
 }
