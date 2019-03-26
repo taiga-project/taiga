e37f7ad29df19797e0bbe6b904396b29d9a915ee
Mátyás ARADI <aradi.matyas@wigner.mta.hu>
2019-03-26 16:49:56 +0000

    Ion position generating on card module added

--- code/running/generate_coords.cu
+++ code/running/generate_coords.cu
@@ -15,7 +15,7 @@ __device__ generate_coords(int idx, double **position_all, double ***speed_all, 
     srand ( time(NULL) );
     
     /* set position of particles */
-    do{
+ /*   do{
         ionisation_yeald = (double)rand()/RAND_MAX;
         position_all[0][idx] = linear_interpolate(prof_d, prof_size[0], prof_r, prof_size[0], ionisation_yeald);
     }while (isnan(XR[i])||XR[i]<0);
@@ -23,15 +23,15 @@ __device__ generate_coords(int idx, double **position_all, double ***speed_all, 
         //if (prof_size[1] <= 0){
             position_all[1][idx]=(double)(rand()-RAND_MAX/2)/RAND_MAX*beam.diameter;
             position_all[2][idx]=(double)(rand()-RAND_MAX/2)/RAND_MAX*beam.diameter;
-        /*}else{
+        }else{
             ionisation_yeald = (double)rand()/RAND_MAX;
             xsec_ang = (double)rand()/RAND_MAX*2*PI;
             xsec_rad = linear_interpolate(profx_d, prof_size[1], profx_r, prof_size[1], ionisation_yeald)*(beam.diameter/2);
             XZ[i]= sin(xsec_ang) * xsec_rad;
             XT[i]= cos(xsec_ang) * xsec_rad;
-        }*/
+        }
     }while ((XZ[i]*XZ[i]+XT[i]*XT[i])>=(beam.diameter/2)*(beam.diameter/2));
-    
+    */
     /* toroidal deflection */
     position_all[2][idx] += tan(beam.toroidal_deflection) * ($R_defl - XR[i]);
     
