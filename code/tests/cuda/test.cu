33e5cc1c4b4c43cf9929394d8bed7e7fab01aa5d
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2021-01-28 22:27:49 +0100

    Update GPU properties test

--- code/tests/cuda/test.cu
+++ code/tests/cuda/test.cu
@@ -6,41 +6,39 @@
 #include <math.h>
 #include <string.h>
 
-
-
 int ConvertSMVer2Cores(int major, int minor)
 {
-        // Defines for GPU Architecture types (using the SM version to determine the # of cores per SM
-        typedef struct {
-                int SM; // 0xMm (hexidecimal notation), M = SM Major version, and m = SM minor version
-                int Cores;
-        } sSMtoCores;
+    // Defines for GPU Architecture types (using the SM version to determine the # of cores per SM
+    typedef struct {
+            int SM; // 0xMm (hexidecimal notation), M = SM Major version, and m = SM minor version
+            int Cores;
+    } sSMtoCores;
 
-        sSMtoCores nGpuArchCoresPerSM[] =   { 
-			{ 0x10,  8 }, // Tesla Generation (SM 1.0) G80 class
-		    { 0x11,  8 }, // Tesla Generation (SM 1.1) G8x class
-		    { 0x12,  8 }, // Tesla Generation (SM 1.2) G9x class
-		    { 0x13,  8 }, // Tesla Generation (SM 1.3) GT200 class
-		    { 0x20, 32 }, // Fermi Generation (SM 2.0) GF100 class
-		    { 0x21, 48 }, // Fermi Generation (SM 2.1) GF10x class
-		    { 0x30, 192}, // Kepler Generation (SM 3.0) GK10x class
-		    { 0x32, 192}, // Kepler Generation (SM 3.2) GK10x class
-		    { 0x35, 192}, // Kepler Generation (SM 3.5) GK11x class
-		    { 0x37, 192}, // Kepler Generation (SM 3.7) GK21x class
-		    { 0x50, 128}, // Maxwell Generation (SM 5.0) GM10x class
-		    { 0x52, 128}, // Maxwell Generation (SM 5.2) GM20x class
-		    {   -1, -1 }
-        };
+    sSMtoCores nGpuArchCoresPerSM[] = {
+        { 0x10,  8 }, // Tesla Generation (SM 1.0) G80 class
+        { 0x11,  8 }, // Tesla Generation (SM 1.1) G8x class
+        { 0x12,  8 }, // Tesla Generation (SM 1.2) G9x class
+        { 0x13,  8 }, // Tesla Generation (SM 1.3) GT200 class
+        { 0x20, 32 }, // Fermi Generation (SM 2.0) GF100 class
+        { 0x21, 48 }, // Fermi Generation (SM 2.1) GF10x class
+        { 0x30, 192}, // Kepler Generation (SM 3.0) GK10x class
+        { 0x32, 192}, // Kepler Generation (SM 3.2) GK10x class
+        { 0x35, 192}, // Kepler Generation (SM 3.5) GK11x class
+        { 0x37, 192}, // Kepler Generation (SM 3.7) GK21x class
+        { 0x50, 128}, // Maxwell Generation (SM 5.0) GM10x class
+        { 0x52, 128}, // Maxwell Generation (SM 5.2) GM20x class
+        {   -1, -1 }
+    };
 
-        int index = 0;
-        while (nGpuArchCoresPerSM[index].SM != -1) {
-                if (nGpuArchCoresPerSM[index].SM == ((major << 4) + minor) ) {
-                        return nGpuArchCoresPerSM[index].Cores;
-                }
-                index++;
-        }
-        printf("MapSMtoCores SM %d.%d is undefined (please update to the latest SDK)!\n", major, minor);
-        return -1;
+    int index = 0;
+    while (nGpuArchCoresPerSM[index].SM != -1) {
+            if (nGpuArchCoresPerSM[index].SM == ((major << 4) + minor) ) {
+                    return nGpuArchCoresPerSM[index].Cores;
+            }
+            ++index;
+    }
+    printf("MapSMtoCores SM %d.%d is undefined (please update to the latest SDK)!\n", major, minor);
+    return -1;
 }
 
 void DisplayHeader()
@@ -56,15 +54,15 @@ void DisplayHeader()
     {
         cudaDeviceProp props;
         cudaGetDeviceProperties(&props, i);
-		//printf("%d: %s\n",i,&props.name);
+        //printf("%d: %s\n",i,&props.name);
      //   wcout << i << ": " << props.name << ": " << props.major << "." << props.minor << endl;
-     	printf("%d: %s: %d.%d\n",i,props.name,props.major,props.minor); 
+         printf("%d: %s: %d.%d\n",i,props.name,props.major,props.minor); 
         printf("  CUDA Cores:\t\t%d (%dx%d)\n",
                ConvertSMVer2Cores(props.major, props.minor) * props.multiProcessorCount,
                props.multiProcessorCount,
                ConvertSMVer2Cores(props.major, props.minor));
 
-		printf("  Global memory:\t%lf MiB\n", (double)props.totalGlobalMem / mb );
+        printf("  Global memory:\t%lf MiB\n", (double)props.totalGlobalMem / mb );
         printf("  Shared memory:\t%lf kiB\n" , (double)props.sharedMemPerBlock / kb );
         printf("  Constant memory:\t%lf kiB\n" , (double)props.totalConstMem / kb );
         printf("  Block registers:\t%d\n" , props.regsPerBlock );
@@ -79,8 +77,8 @@ void DisplayHeader()
 }
 
 int main(){
-        DisplayHeader();
-        return 0;
- }
+    DisplayHeader();
+    return 0;
+}
 
 
