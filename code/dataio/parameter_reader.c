78798b0d995ea9bb789267a16c7df66d4d8ace45
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2021-01-29 01:22:16 +0100

    #7 Minor changes

--- code/dataio/parameter_reader.c
+++ code/dataio/parameter_reader.c
@@ -16,7 +16,7 @@ void init_taiga_props(char* par_name, char* par_value, BeamProp *beam, ShotProp 
     
     double par_value_lf;    sscanf(par_value, "%lf", &par_value_lf);
     int par_value_d;        sscanf(par_value, "%d", &par_value_d);
-        
+    
     if (!strcmp(par_name, "shotnumber"))                    strcpy(shot->shotnumber, clean_string(par_value));
     else if (!strcmp(par_name, "time"))                     strcpy(shot->time, clean_string(par_value));
     else if (!strcmp(par_name, "runnumber"))                strcpy(run->runnumber, clean_string(par_value));
@@ -34,12 +34,12 @@ void init_taiga_props(char* par_name, char* par_value, BeamProp *beam, ShotProp 
     else if (!strcmp(par_name, "diameter"))                 beam->diameter = par_value_lf/1000.0;
     else if (!strcmp(par_name, "detector"))                 strcpy(shot->detector_geometry, clean_string(par_value));
     else if (!strcmp(par_name, "detector_mask"))            strcpy(shot->detector_mask, clean_string(par_value));
-    else if (!strcmp(par_name, "electric_field_module"))    shot->electric_field_module = par_value_d;    
+    else if (!strcmp(par_name, "electric_field_module"))    shot->electric_field_module = par_value_d;
     else if (!strcmp(par_name, "timestep"))                 run->timestep = par_value_lf;
     else if (!strcmp(par_name, "step_device"))              run->step_device = par_value_d;
     else if (!strcmp(par_name, "step_host"))                run->step_host = par_value_d;
     else if (!strcmp(par_name, "particles")){
-        run->particle_number = par_value_d;        
+        run->particle_number = par_value_d;
         run->block_number    = par_value_d/run->block_size+1;
     }
 }
@@ -50,7 +50,7 @@ int parameter_reader(BeamProp *beam, ShotProp *shot, RunProp *run){
     char str[MAXCHAR];
     char* par_name;
     char* par_value;
- 
+    
     fp = fopen(run->parameter_file, "r");
     if (fp == NULL){
         printf("Could not open file %s",run->parameter_file);
@@ -62,13 +62,12 @@ int parameter_reader(BeamProp *beam, ShotProp *shot, RunProp *run){
             par_value = strtok(NULL, "#");
             init_taiga_props(par_name, par_value, beam, shot, run);
         }
-    }    
+    }
     fclose(fp);
     strcpy(shot->name,concat(shot->shotnumber,"_",shot->time));
     strcpy(run->folder_out,concat("results/",shot->shotnumber,"_",shot->time));
     return 0;
 }
-
 
 int runnumber_reader(ShotProp *shot, RunProp *run){
     if (strcmp(run->runnumber,"0") == 0){
@@ -111,12 +110,12 @@ double get_mass(char *s){
         }catch (...){
             mass = 7.016004558;
         }
-    }    
+    }
     return mass;
 }
 
 void set_detector_geometry(double *DETECTOR, char* values){
-    char *el; 
+    char *el;
     el = strtok(values,",");    DETECTOR[0] = strtod (el, NULL);
     el = strtok(NULL,",");      DETECTOR[1] = strtod (el, NULL);
     el = strtok(NULL,",");      DETECTOR[2] = tan(strtod (el, NULL) * PI/180.0);
