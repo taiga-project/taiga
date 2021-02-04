a7d0a1434f7dcf4c8289ee81d35fc15537f55ba5
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2021-02-04 20:10:14 +0100

    #18 Update debug functions

--- code/debug_functions.h
+++ code/debug_functions.h
@@ -1,3 +1,3 @@
-void debug_message_init(double* XR, double* XZ, double* XT, double* VR, double* VZ, double* VT);
-void debug_message_run(double* XR, double* XZ, double* XT, double* VR, double* VZ, double* VT);
+void debug_message_init(TaigaGlobals *g);
+void debug_message_run(TaigaGlobals *g);
 void debug_service_vars(double *SERVICE_VAR);
