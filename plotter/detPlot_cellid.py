ce81bd473c1e3e1d6c4a6a3bfbaebf686b40a5c4
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2021-02-24 23:37:06 +0100

    #20 Update detector plotter: visualise relative current on each cells

--- plotter/detPlot_cellid.py
+++ plotter/detPlot_cellid.py
@@ -1,112 +1,76 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as pl
+import matplotlib.patches as patches
+import matplotlib.cm as colormap
 import scipy.stats as st
 
-max_distance = 1e-4
+try:
+    shotnumber = sys.argv[1];
+except:
+    print 'Shot number is not given as proper input parameter. Default value (17178) is used.'
+    shotnumber = '17178'
 
 try:
-	shotnumber = sys.argv[1];
+    time = sys.argv[2];
 except:
-	print 'Shot number is not given as proper input parameter. Default value (17178) is used.'
-	shotnumber = '17178'
+    print 'Time is not given as proper input parameter. Default value (1005) is used.'
+    time = '1005'
 
 try:
-	time = sys.argv[2];
+    runnumber = sys.argv[3];
 except:
-	print 'Time is not given as proper input parameter. Default value (1005) is used.'
-	time = '1005'
+    print 'Runnumber is not given as proper input parameter. Default value (402) is used.'
+    runnumber = '402'
 
-try:
-	runnumber = sys.argv[3];
-except:
-	print 'Runnumber is not given as proper input parameter. Default value (402) is used.'
-	runnumber = '402'
-
-try:
-	detector_par = sys.argv[4];
-except:
-	print 'Detector parameters is not given as proper input parameter. Default value (0.685,0.22,0,38,0) is used.'
-	detector_par = '0.685,0.22,0,38,0'
-
-detector_par_list = detector_par.split(',')
-
-try:
-	R0 = float(detector_par_list[0]);
-except:
-	R0 = 0.685
-
-try:
-	Z0 = float(detector_par_list[1]);
-except:
-	Z0 = 0.23
-
-try:
-	T0 = float(detector_par_list[2]);
-except:
-	T0 = 0.0
-
-try:
-	detector_RZ_angle = float(detector_par_list[3]);
-except:
-	detector_RZ_angle = 38.0
-
-try:
-	detector_ZT_angle = float(detector_par_list[4]);
-except:
-	detector_ZT_angle = 0.0
-	
 results_folder = 'results/'+shotnumber+'_'+time+'/'+runnumber+'/'
 
 try:
-	C = np.loadtxt(results_folder+'detector/cellcounter.dat')
-	X = np.loadtxt(results_folder+'detector/detx')
-	Y = np.loadtxt(results_folder+'detector/dety')
+    C = np.loadtxt(results_folder+'detector/cellcounter.dat')
+    X = np.loadtxt(results_folder+'detector/detx')
+    Y = np.loadtxt(results_folder+'detector/dety')
 except:
-	print 'Invalid input folder: '+results_folder
+    print 'Invalid input folder: '+results_folder
+
+
+xmin = X[0,0]
+xmax = X[-1,-1]
+ymin = Y[0,0]
+ymax = Y[-1,-1]
+xmean = (X[:,0]+X[:,1])/2
+ymean = (Y[:,0]+Y[:,1])/2
+cmax = C.max()
 
 try:
-	
-#	x = T[particle_on_detector]
-#	y = Z[particle_on_detector]
-#	xmin, xmax = -4e-2, 4e-2
-#	ymin, ymax = -4e-2, 4e-2
-	
-	fig = pl.figure()
-	ax = fig.gca()
-#	ax.set_xlim(xmin, xmax)
-#	ax.set_ylim(ymin, ymax)
-	ax.set_aspect('equal')
-	
-	# Contourf plot
-#	pl.contourf(X[:,0],Y[::-1,0],C, cmap='afmhot')
-	pl.contourf(C, cmap='afmhot')
-	print(C)
-	pl.xlabel(r"$x \mathrm{ [mm]}$")
-	pl.ylabel(r"$y \mathrm{ [mm]}$")
+    fig = pl.figure()
+    ax = fig.gca()
+    ax.set_xlim(xmin, xmax)
+    ax.set_ylim(ymin, ymax)
+    ax.set_aspect('equal')
+
+    # Contourf plot
+    pl.contourf(xmean, ymean, C, cmap='afmhot', corner_mask=False)
+
+    # Detector cell matrix plot
+    for i in range (X[:,0].size):
+        for j in range (Y[:,0].size):
+            rect = patches.Rectangle((X[i,0], Y[j,0]), X[i,1]-X[i,0], Y[j,1]-Y[j,0], linewidth=1,
+                                     edgecolor='k', facecolor=colormap.afmhot(C[j,i]/cmax))
+            ax.add_patch(rect)
+
+    print(C)
+
+    pl.xlabel(r"$x \mathrm{ [mm]}$")
+    pl.ylabel(r"$y \mathrm{ [mm]}$")
+    pl.title(r"COMPASS #"+shotnumber+" (t="+time+" s)")
+
+    try:
+        print 'Save plot to '+results_folder+'detpy2_'+shotnumber+'_'+time+'.pdf'
+        pl.savefig(results_folder+'detpy2_'+shotnumber+'_'+time+'.pdf')
+        pl.clf()
+    except:
+        print 'Unable to save to : '+results_folder
+        pl.show()
 except:
-	print 'Unable to plot: '+results_folder
+    print 'Unable to plot: '+results_folder
 
-try:
-	print 'Save plot to '+results_folder+'detpy2_'+shotnumber+'_'+time+'.pdf'
-	pl.savefig(results_folder+'detpy2_'+shotnumber+'_'+time+'.pdf')
-	pl.clf()
-
-except:
-	print 'Unable to save to : '+results_folder
-	pl.show()
-
-xmin, xmax = -4e-2, 4e-2
-ymin, ymax = -4e-2, 4e-2
-
-#x = T[particle_on_detector]-T0
-#y = (Z[particle_on_detector]-Z0) / np.sin( (detector_RZ_angle/180.0)*np.pi )
-#nbins=250
-#fig = pl.figure()
-#ax = fig.gca()
-#ax.set_xlim(xmin, xmax)
-#ax.set_ylim(ymin, ymax)
-#ax.set_aspect('equal')
-#cfset = ax.hist2d(x, y, bins=nbins, range = [[xmin,xmax],[ymin,ymax]], cmap='afmhot')
-#pl.colorbar(cfset[3]),ax=ax)
-#pl.show()
