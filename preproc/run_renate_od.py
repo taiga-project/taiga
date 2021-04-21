614ad45145b4badcba40431df644b26f7d435ff0
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2021-04-21 18:57:06 +0200

    #45 Add param setter without input xml and refactor

--- preproc/run_renate_od.py
+++ preproc/run_renate_od.py
@@ -1,35 +1,57 @@
 from crm_solver.beamlet import Beamlet
 import pandas
 import numpy
+import lxml
 
 
-components = pandas.DataFrame(
-    {'q': [-1, 1], 'Z': [0, 1], 'A': [0, 2]},
-    index=['electron', 'ion'])
+def get_param():
+    xml_content = '<xml lang="en"><head><id>taiga beamlet</id></head><body>' \
+                  '<beamlet_energy unit = "keV">60</beamlet_energy>' \
+                  '<beamlet_species unit = "">Na</beamlet_species>' \
+                  '<beamlet_current unit = "A">0.001</beamlet_current>' \
+                  '</body></xml>'
+    xml_root = lxml.etree.XML(xml_content)
+    return lxml.etree.ElementTree(xml_root)
 
 
-tuples = [('beamlet grid', 'distance', 'm'),
-          ('electron', 'density', 'm-3'),
-          ('electron', 'temperature', 'eV'),
-          ('ion1', 'density', 'm-3'),
-          ('ion1', 'temperature', 'eV')]
+def get_components():
+    components = pandas.DataFrame(
+        {'q': [-1, 1], 'Z': [0, 1], 'A': [0, 2]},
+        index=['electron', 'ion'])
+    return components
 
-header = pandas.MultiIndex.from_tuples(tuples, names=['type', 'property', 'unit'])
 
-distance = [0, 0.1, 0.2]
-density = [1e19, 1.2e19, 1.5e19]
-temperature = [1000, 1500, 2500]
-profiles_data = numpy.transpose(numpy.array([distance, density, temperature, density, temperature]))
-profiles = pandas.DataFrame(profiles_data, columns=header)
+def get_profiles():
+    tuples = [('beamlet grid', 'distance', 'm'),
+              ('electron', 'density', 'm-3'),
+              ('electron', 'temperature', 'eV'),
+              ('ion1', 'density', 'm-3'),
+              ('ion1', 'temperature', 'eV')]
 
-b = Beamlet(profiles=profiles, components=components, data_path="beamlet/taiga_beamlet.xml")
-pops = b.profiles.filter(like='level', axis=1)
-reference_pop = pops.iat[0, 0]
-rates = pops/reference_pop
-ionisation_degree = 1-rates.sum(1)
-print(ionisation_degree)
+    header = pandas.MultiIndex.from_tuples(tuples, names=['type', 'property', 'unit'])
 
-b.profiles.to_html('data/output/matyi/temp.html')
-b.profiles.to_csv('data/output/matyi/temp.txt')
-b.profiles['beamlet grid'].to_csv('data/output/matyi/rad.txt', index=False, header=False)
-ionisation_degree.to_csv('data/output/matyi/degree.txt', index=False, header=False)
+    distance = [0, 0.1, 0.2]
+    density = [1e19, 1.2e19, 1.5e19]
+    temperature = [1000, 1500, 2500]
+    profiles_data = numpy.transpose(numpy.array([distance, density, temperature, density, temperature]))
+    profiles = pandas.DataFrame(profiles_data, columns=header)
+
+    return profiles
+
+
+def export_beamlet_profile():
+    b = Beamlet(param=get_param(), profiles=get_profiles(), components=get_components())
+    pops = b.profiles.filter(like='level', axis=1)
+    reference_pop = pops.iat[0, 0]
+    rates = pops / reference_pop
+    ionisation_degree = 1 - rates.sum(1)
+    print(ionisation_degree)
+
+    b.profiles.to_html('data/output/matyi/temp.html')
+    b.profiles.to_csv('data/output/matyi/temp.txt')
+    b.profiles['beamlet grid'].to_csv('data/output/matyi/rad.txt', index=False, header=False)
+    ionisation_degree.to_csv('data/output/matyi/degree.txt', index=False, header=False)
+
+
+if __name__ == "__main__":
+    export_beamlet_profile()
