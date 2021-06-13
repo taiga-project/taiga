3e1e5a9b291f6172acbc9f905e7ee75b1f3046c6
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2021-06-14 00:16:55 +0200

    #77 Fix: B-spline and spline coefficients were mixed

--- src/core/localise_field.cu
+++ src/core/localise_field.cu
@@ -7,7 +7,7 @@ __device__ void (*get_coefficients)(TaigaCommons *c,
                                     double *local_spline_erad, double *local_spline_ez, double *local_spline_etor,
                                     double *local_polflux, int zgrid_length);
 
-__device__ void get_coefficients_with_splines(
+__device__ void get_coefficients_with_bsplines(
                                 TaigaCommons *c,
                                 const int *local_spline_indices,
                                 double *local_spline_brad, double *local_spline_bz, double *local_spline_btor,
@@ -33,7 +33,7 @@ __device__ void get_coefficients_with_splines(
     }
 }
 
-__device__ void get_coefficients_with_bsplines(
+__device__ void get_coefficients_with_splines(
         TaigaCommons *c,
         const int *local_spline_indices,
         double *local_spline_brad, double *local_spline_bz, double *local_spline_btor,
