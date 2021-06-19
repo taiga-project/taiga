384656b6e74b6975c168306f91b76693b94188db
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2021-06-19 23:46:40 +0200

    #82 Fix init test

---
+++ src/tests/helper.cuh
@@ -0,0 +1,9 @@
+#ifndef TEST_HELPER_CUH
+#define TEST_HELPER_CUH
+
+void init_tmp(double *h_tmp);
+void print_tmp(double *h_tmp);
+void start_reference(double **h_tmp, double **d_tmp);
+void end_reference(double **h_tmp, double **d_tmp);
+
+#endif //TEST_HELPER_CUH
