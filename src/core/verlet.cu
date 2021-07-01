5a0f51fd868e151510fe95fce777ea763245824a
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2021-07-01 18:42:25 +0200

    #54 Fix Verlet method and modify to BGSDC(K. Tretiak and D. Ruprecht, Journal of Computational Physics: X, Volume 4, September 2019, 100036)

--- src/core/verlet.cu
+++ src/core/verlet.cu
@@ -1,18 +1,52 @@
 // velocity Verlet
-
 #include "solvers.cuh"
 
-__device__ void solve_diffeq_by_verlet(double *X, double *a, double *B, double *E, double eperm, double timestep){
-    double R = X[0] + X[3]*timestep + 0.5*a[0]*timestep*timestep;
-    double Z = X[1] + X[4]*timestep + 0.5*a[1]*timestep*timestep;
-    double T = X[2] + X[5]*timestep + 0.5*a[2]*timestep*timestep;
-    double new_a[3];
-    (*get_acceleration_from_lorentz_force)(new_a, &X[3], B, E, eperm);
-    X[0] = R;
-    X[1] = Z;
-    X[2] = T;
-    X[3] += (a[0] + new_a[0])*timestep*0.5;
-    X[4] += (a[1] + new_a[1])*timestep*0.5;
-    X[5] += (a[2] + new_a[2])*timestep*0.5;
-    memcpy(a, new_a, 3*sizeof(double));
+__device__ double cross(double *u, double *v, int index) {
+    switch(index){
+        case 0:
+            return u[1]*v[2] - u[2]*v[1];
+        case 1:
+            return u[2]*v[0] - u[0]*v[2];
+        case 2:
+            return u[0]*v[1] - u[1]*v[0];
+    }
+}
+
+__device__ void solve_diffeq_by_verlet(double *X, double *X_prev, double *B, double *E,  double *E_prev, double eperm, double timestep) {
+    int i;
+    double a[3];
+
+    double t[3];
+    double v_minus[3];
+    double v_star[3];
+    double v_plus[3];
+
+    double t_sq = 0;
+    double s__t;
+
+    double dt__2 = 0.5 * timestep;
+
+    for (i = 0; i < 3; ++i) {
+        t[i] = eperm * dt__2 * B[i];
+        t_sq += t[i] * t[i];
+    }
+    s__t = 2.0/(1.0+t_sq);
+
+    (*get_acceleration_from_lorentz_force)(a, &X_prev[3], B, E, eperm);
+
+    for (i = 0; i < 3; ++i) {
+        X[i] += timestep * (X_prev[i+3] + dt__2*a[i]);
+        E[i] = 0.5 * (E[i] + E_prev[i]);
+        v_minus[i] = X_prev[i+3] + dt__2*E[i];
+    }
+
+    for (i = 0; i < 3; ++i) {
+        v_star[i] = v_minus[i] + cross(v_minus, t, i);
+    }
+
+    for (i = 0; i < 3; ++i) {
+        v_plus[i] = v_minus[i] + s__t * cross(v_star, t, i);
+        X[i+3] = v_plus[i] + dt__2 * E[i];
+    }
+
 }
