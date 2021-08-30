f3c45da4a3ca202a1bb9ca3e2c547d3cd907c07e
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2021-08-31 00:34:50 +0200

    #116 Minor refactor

--- src/utils/physics.c
+++ src/utils/physics.c
@@ -2,23 +2,23 @@
 #include "physics.h"
 
 //https://physics.nist.gov/cgi-bin/Compositions/stand_alone.pl
-double get_mass(char *name_of_ion, int charge){
-    if (!strcmp(name_of_ion,"D") || !strcmp(name_of_ion,"H2")){
+double get_mass(char *name_of_ion, int charge) {
+    if (!strcmp(name_of_ion, "D") || !strcmp(name_of_ion, "H2")) {
         return 2.0141017781212 - charge * MASS_OF_ELECTRON;
-    }else if (!strcmp(name_of_ion,"Li") || !strcmp(name_of_ion,"Li7")){
+    } else if (!strcmp(name_of_ion, "Li") || !strcmp(name_of_ion, "Li7")) {
         return 7.016003436645 - charge * MASS_OF_ELECTRON;
-    }else if (!strcmp(name_of_ion,"Na") || !strcmp(name_of_ion,"Na23")){
+    } else if (!strcmp(name_of_ion, "Na") || !strcmp(name_of_ion, "Na23")) {
         return 22.989769282019 - charge * MASS_OF_ELECTRON;
-    }else if (!strcmp(name_of_ion,"K") || !strcmp(name_of_ion,"K39")){
+    } else if (!strcmp(name_of_ion, "K") || !strcmp(name_of_ion, "K39")) {
         return 38.963706486449 - charge * MASS_OF_ELECTRON;
-    }else if (!strcmp(name_of_ion,"Rb") || !strcmp(name_of_ion,"Rb85")){
+    } else if (!strcmp(name_of_ion, "Rb") || !strcmp(name_of_ion, "Rb85")) {
         return 84.911789737954 - charge * MASS_OF_ELECTRON;
-    }else if (!strcmp(name_of_ion,"Cs") || !strcmp(name_of_ion,"Cs133")){
+    } else if (!strcmp(name_of_ion, "Cs") || !strcmp(name_of_ion, "Cs133")) {
         return 132.905451961080 - charge * MASS_OF_ELECTRON;
-    }else{
-        try{
+    } else {
+        try {
             return atof(name_of_ion);
-        }catch (...){
+        } catch (...) {
             printf("Error: Invalid ion species: %s\n", name_of_ion);
             exit(1);
         }
@@ -26,7 +26,7 @@ double get_mass(char *name_of_ion, int charge){
 }
 
 //http://www-personal.umich.edu/~cowley/ionen.htm
-double get_ionisation_energy(char *name_of_ion, int charge){
+double get_ionisation_energy(char *name_of_ion, int charge) {
     switch (charge) {
         case 0:
             if (!strcmp(name_of_ion, "Li") || !strcmp(name_of_ion, "Li7")) {
@@ -40,7 +40,8 @@ double get_ionisation_energy(char *name_of_ion, int charge){
             } else if (!strcmp(name_of_ion, "Cs") || !strcmp(name_of_ion, "Cs133")) {
                 return 3.89390;
             } else {
-                printf("Warning: Invalid ion species: %s\n         Secondary ionisation module turned off", name_of_ion);
+                printf("Warning: Invalid ion species: %s\n         Secondary ionisation module turned off",
+                       name_of_ion);
                 return INFINITY;
             }
         case 1:
@@ -55,7 +56,8 @@ double get_ionisation_energy(char *name_of_ion, int charge){
             } else if (!strcmp(name_of_ion, "Cs") || !strcmp(name_of_ion, "Cs133")) {
                 return 23.15744;
             } else {
-                printf("Warning: Invalid ion species: %s\n         Secondary ionisation module turned off", name_of_ion);
+                printf("Warning: Invalid ion species: %s\n         Secondary ionisation module turned off",
+                       name_of_ion);
                 return INFINITY;
             }
         default:
