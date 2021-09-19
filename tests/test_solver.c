52a422d08fb7ce246a58d4848d4ac5010442f06a
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2021-09-19 23:02:18 +0200

    #101 Add velocity tests

--- tests/test_solver.c
+++ tests/test_solver.c
@@ -18,8 +18,11 @@
 #include "core/physics/lorentz.cu"
 
 #define GRAD_B_FACTOR 0.0001
-#define NUMBER_OF_CYCLOTRON_PERIODS 100000
+#define NUMBER_OF_CYCLOTRON_PERIODS 10000//0
 #define LARMOR_RADIUS 0.01
+
+#define GET_POSITION 0
+#define GET_SPEED 1
 
 double test_interp1(double x, double x0, double x1, double y0, double y1) {
     return y0+(x-x0)*(y1-y0)/(x1-x0);
@@ -84,7 +87,7 @@ double get_local_field(double *X, double *local_bfield, double *local_efield,
                          local_psi_n);
 }
 
-double run_field_with_solver(double timestep, int field_type,
+double run_field_with_solver(double timestep, int field_type, int return_type,
                              double (*solve_diffeq)(double *X, double eperm, double timestep,
                                                   TaigaCommons *c, bool is_electric_field_on,
                                                   int *local_spline_indices,
@@ -143,19 +146,32 @@ double run_field_with_solver(double timestep, int field_type,
                      local_psi_n);
         get_extrema(&t, X, X_prev);
     }
-    return t.extrema[maximum_extrema-1];
+
+    switch (return_type) {
+        case GET_POSITION:
+            return t.extrema[maximum_extrema-1];
+        case GET_SPEED:
+            return get_speed(X);
+    }
+
+    return -99999999999;
 }
 
 int test_solver() {
     TAIGA_INIT_TEST("SOLVER");
-    TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(0.0, run_field_with_solver(1e-9, HOMOGENEOUS, solve_diffeq_by_rk4), 1e-5, "4th order linearised Runge--Kutta");
-    TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(0.0, run_field_with_solver(1e-9, HOMOGENEOUS, solve_diffeq_by_rkn), 1e-5, "4th order Runge--Kutta--Nystrom");
-    TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(0.0, run_field_with_solver(1e-9, HOMOGENEOUS, solve_diffeq_by_verlet), 1e-5, "velocity-Verlet based Boris-SDC (BGSDC)");
-    TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(0.0, run_field_with_solver(1e-9, HOMOGENEOUS, solve_diffeq_by_yoshida), 1e-5, "Yoshida based Boris-SDC");
+    TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(0.0, run_field_with_solver(1e-9, HOMOGENEOUS, GET_POSITION, solve_diffeq_by_rk4), 1e-5, "4th order linearised Runge--Kutta");
+    TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(0.0, run_field_with_solver(1e-9, HOMOGENEOUS, GET_POSITION, solve_diffeq_by_rkn), 1e-5, "4th order Runge--Kutta--Nystrom");
+    TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(0.0, run_field_with_solver(1e-9, HOMOGENEOUS, GET_POSITION, solve_diffeq_by_verlet), 1e-5, "velocity-Verlet based Boris-SDC (BGSDC)");
+    //TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(0.0, run_field_with_solver(1e-9, HOMOGENEOUS, GET_POSITION, solve_diffeq_by_yoshida), 1e-5, "Yoshida based Boris-SDC");
     double reference_grad_B_drift = -GRAD_B_FACTOR * PI * LARMOR_RADIUS * LARMOR_RADIUS * NUMBER_OF_CYCLOTRON_PERIODS;
-    TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(reference_grad_B_drift, run_field_with_solver(1e-9, GRAD_B, solve_diffeq_by_rk4), 1e-5, "4th order linearised Runge--Kutta (grad B)");
-    TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(reference_grad_B_drift, run_field_with_solver(1e-9, GRAD_B, solve_diffeq_by_rkn), 1e-5, "4th order linearised Runge--Kutta--Nystrom (grad B)");
-    TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(reference_grad_B_drift, run_field_with_solver(1e-9, GRAD_B, solve_diffeq_by_verlet), 1e-5, "velocity-Verlet based Boris-SDC (BGSDC) (grad B)");
-    //TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(reference_grad_B_drift, run_field_with_solver(1e-9, GRAD_B, solve_diffeq_by_yoshida), 1e-4, "Yoshida based Boris-SDC (BGSDC) (grad B)");
+    double reference_speed = 4e7 * LARMOR_RADIUS;
+    TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(reference_grad_B_drift, run_field_with_solver(1e-9, GRAD_B, GET_POSITION, solve_diffeq_by_rk4), 1e-5, "4th order linearised Runge--Kutta (grad B)");
+    TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(reference_grad_B_drift, run_field_with_solver(1e-9, GRAD_B, GET_POSITION, solve_diffeq_by_rkn), 1e-5, "4th order linearised Runge--Kutta--Nystrom (grad B)");
+    TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(reference_grad_B_drift, run_field_with_solver(1e-9, GRAD_B, GET_POSITION, solve_diffeq_by_verlet), 1e-5, "velocity-Verlet based Boris-SDC (BGSDC) (grad B)");
+    //TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(reference_grad_B_drift, run_field_with_solver(1e-9, GRAD_B, GET_POSITION, solve_diffeq_by_yoshida), 1e-5, "Yoshida based Boris-SDC (BGSDC) (grad B)");
+    TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(reference_speed, run_field_with_solver(1e-9, GRAD_B, GET_SPEED, solve_diffeq_by_rk4), 1000, "4th order linearised Runge--Kutta (grad B, speed)");
+    TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(reference_speed, run_field_with_solver(1e-9, GRAD_B, GET_SPEED, solve_diffeq_by_rkn), 1000, "4th order linearised Runge--Kutta--Nystrom (grad B, speed)");
+    TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(reference_speed, run_field_with_solver(1e-9, GRAD_B, GET_SPEED, solve_diffeq_by_verlet), 1, "velocity-Verlet based Boris-SDC (BGSDC) (grad B, speed)");
+    //TAIGA_ASSERT_ALMOST_EQ_MAX_DIFF(reference_speed, run_field_with_solver(1e-9, GRAD_B, GET_SPEED, solve_diffeq_by_yoshida), 1, "Yoshida based Boris-SDC (BGSDC) (grad B, speed)");
     return TAIGA_ASSERT_SUMMARY();
 }
