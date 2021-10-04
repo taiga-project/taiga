954f4255a8ba1fc08e1152cf65a0040b45e51557
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2021-10-05 01:01:12 +0200

    #138 Fix linear_interpolate()

--- src/utils/basic_functions.c
+++ src/utils/basic_functions.c
@@ -6,36 +6,40 @@
 #include <ctype.h>
 #include "utils/basic_functions.h"
 
-double linear_interpolate(double *x_vector, long x_length, double *y_vector, long y_length, double x_value){
+double linear_interpolate(double *x_vector, long x_length, double *y_vector, long y_length, double x_value) {
     int i;
-    if (x_length != y_length)   printf("ERROR: in interpolation. Two input vectors have different length.");
-    for (i = 0; (i < x_length-1) && (x_vector[i] > x_value); ++i);
-    if(i>1){--i;}else{i=1;}
+    if (x_length != y_length) printf("ERROR: in interpolation. Two input vectors have different length.");
+    if (x_vector[0] > x_vector[x_length-1]) {
+        for (i = 0; (i < x_length - 1) && (x_vector[i + 1] > x_value); ++i);
+    } else {
+        for (i = 0; (i < x_length - 1) && (x_vector[i + 1] < x_value); ++i);
+    }
+    if (x_vector[i+1] == x_vector[i]) printf("ERROR: in interpolation. x0 = x1 (%lf, %lf)\n", x_vector[i+1], x_value);
     return y_vector[i] + (y_vector[i+1]-y_vector[i])*(x_value-x_vector[i])/(x_vector[i+1]-x_vector[i]);
 }
 
-char* concat(const char *s1, ...){
+char* concat(const char *s1, ...) {
     const char *s;
     va_list args;
     char *r;
     size_t arg_length = 1;
     
     va_start(args, s1);
-    for(s=s1; s!=NULL; s=va_arg(args, const char*)){
+    for(s=s1; s!=NULL; s=va_arg(args, const char*)) {
         arg_length += strlen(s);
     }
     va_end(args);
     r = (char*)malloc(arg_length);
     r[0] = NULL;
     va_start(args, s1);
-    for(s=s1; s!=NULL; s=va_arg(args, const char*)){
+    for(s=s1; s!=NULL; s=va_arg(args, const char*)) {
         strcat(r, s);
     }
     va_end(args);
     return r;
 }
 
-void string_to_lowercase(char* str){
+void string_to_lowercase(char* str) {
     char *p;
     for(p=str; *p; ++p) *p=tolower(*p);
 }
