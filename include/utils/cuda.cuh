84903c838c1386a4b9b6175b203f0a6ce522f136
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2022-05-20 17:47:29 +0200

    #22 Add error management, move cuda functions

---
+++ include/utils/cuda.cuh
@@ -0,0 +1,14 @@
+#ifndef TAIGA_CUDA_CUH
+#define TAIGA_CUDA_CUH
+
+#define CHECK_ERROR(cuda_code) { manage_cuda_error((cuda_code), __FILE__, __LINE__); }
+
+void set_cuda(int debug_flag);
+
+inline void manage_cuda_error(cudaError_t cuda_code, const char *filename, int line, bool abort=true) {
+    if (cuda_code != cudaSuccess) {
+        fprintf(stderr,"CUDA ERROR in %s (line %d):\n %s\n", filename, line, cudaGetErrorString(code));
+    }
+}
+
+#endif //TAIGA_CUDA_CUH
