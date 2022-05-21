0deb88000893b359780a82f946a8cf4e976ba7ac
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2022-05-21 03:34:41 +0200

    #22 Collect free together

--- include/utils/free.cuh
+++ include/utils/free.cuh
@@ -1,7 +1,16 @@
 #ifndef TAIGA_FREE_CUH
 #define TAIGA_FREE_CUH
 
+#define HOST 0
+#define DEVICE 1
+#define FREE(where, p) { \
+    if (where == DEVICE) {CHECK_ERROR(cudaFree(p));} \
+    else {free(p);} \
+}
 
-void free_common(TaigaCommons *shared_common);
+void free_detector(DetectorProp *detector, int where);
+void free_beam(BeamProfile *beam_profile, int where);
+void free_global(TaigaGlobals *taiga_global, int where);
+void free_common(TaigaCommons *shared_common, int where);
 
 #endif //TAIGA_FREE_CUH
--- src/utils/free.cu
+++ src/utils/free.cu
@@ -1,52 +1,49 @@
 #include "utils/free.cuh"
 
-
-
-
 void free_taiga() {
 
 }
 
 
-void free_detector(DetectorProp *detector){
-    CHECK_ERROR(cudaFree(detector->counter));
-    CHECK_ERROR(cudaFree(detector->xgrid));
-    CHECK_ERROR(cudaFree(detector->ygrid));
+void free_detector(DetectorProp *detector, int where){
+    FREE(where, detector->counter);
+    FREE(where, detector->xgrid);
+    FREE(where, detector->ygrid);
 }
 
-void free_beam(BeamProfile *beam_profile){
-    CHECK_ERROR(cudaFree(beam_profile->radial_grid));
-    CHECK_ERROR(cudaFree(beam_profile->radial_profile));
-    CHECK_ERROR(cudaFree(beam_profile->cross_grid));
-    CHECK_ERROR(cudaFree(beam_profile->cross_profile));
+void free_beam(BeamProfile *beam_profile, int where){
+    FREE(where, beam_profile->radial_grid);
+    FREE(where, beam_profile->radial_profile);
+    FREE(where, beam_profile->cross_grid);
+    FREE(where, beam_profile->cross_profile);
 }
 
-void free_global(TaigaGlobals *taiga_global){
-    CHECK_ERROR(cudaFree(taiga_global->rad));
-    CHECK_ERROR(cudaFree(taiga_global->z));
-    CHECK_ERROR(cudaFree(taiga_global->tor));
-    CHECK_ERROR(cudaFree(taiga_global->vrad));
-    CHECK_ERROR(cudaFree(taiga_global->vz));
-    CHECK_ERROR(cudaFree(taiga_global->vtor));
-    CHECK_ERROR(cudaFree(taiga_global->detcellid));
-    CHECK_ERROR(cudaFree(taiga_global->intensity));
-    CHECK_ERROR(cudaFree(taiga_global->time_of_flight));
+void free_global(TaigaGlobals *taiga_global, int where){
+    FREE(where, taiga_global->rad);
+    FREE(where, taiga_global->z);
+    FREE(where, taiga_global->tor);
+    FREE(where, taiga_global->vrad);
+    FREE(where, taiga_global->vz);
+    FREE(where, taiga_global->vtor);
+    FREE(where, taiga_global->detcellid);
+    FREE(where, taiga_global->intensity);
+    FREE(where, taiga_global->time_of_flight);
 }
 
-void free_common(TaigaCommons *shared_common){
-    CHECK_ERROR(cudaFree(shared_common->grid_size));
-    CHECK_ERROR(cudaFree(shared_common->spline_rgrid));
-    CHECK_ERROR(cudaFree(shared_common->spline_zgrid));
-    CHECK_ERROR(cudaFree(shared_common->brad));
-    CHECK_ERROR(cudaFree(shared_common->bz));
-    CHECK_ERROR(cudaFree(shared_common->btor));
-    CHECK_ERROR(cudaFree(shared_common->erad));
-    CHECK_ERROR(cudaFree(shared_common->ez));
-    CHECK_ERROR(cudaFree(shared_common->etor));
-    CHECK_ERROR(cudaFree(shared_common->psi_n));
-    CHECK_ERROR(cudaFree(shared_common->detector_geometry));
-    CHECK_ERROR(cudaFree(shared_common->ts_psi));
-    CHECK_ERROR(cudaFree(shared_common->ts_density));
-    CHECK_ERROR(cudaFree(shared_common->ts_temperature));
+void free_common(TaigaCommons *shared_common, int where){
+    FREE(where, shared_common->grid_size);
+    FREE(where, shared_common->spline_rgrid);
+    FREE(where, shared_common->spline_zgrid);
+    FREE(where, shared_common->brad);
+    FREE(where, shared_common->bz);
+    FREE(where, shared_common->btor);
+    FREE(where, shared_common->erad);
+    FREE(where, shared_common->ez);
+    FREE(where, shared_common->etor);
+    FREE(where, shared_common->psi_n);
+    FREE(where, shared_common->detector_geometry);
+    FREE(where, shared_common->ts_psi);
+    FREE(where, shared_common->ts_density);
+    FREE(where, shared_common->ts_temperature);
 }
 
