d3c035793085450d0fdbce4be780f4f99e95fb2c
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2023-12-02 20:29:31 +0100

    Update path

--- preproc/renate_od/interface.py
+++ preproc/renate_od/interface.py
@@ -6,7 +6,7 @@ from efit import EFITManager
 from utils import *
 
 
-def export_beamlet_profile(export_root=get_home_directory() + '/input/ionProf/',
+def export_beamlet_profile(export_root=os.path.join(get_home_directory(), 'input', 'ionProf'),
                            shot_number='17178', time='1097', species='Li', energy='80'):
     z = 0
     tor = 0
@@ -14,23 +14,21 @@ def export_beamlet_profile(export_root=get_home_directory() + '/input/ionProf/',
     r = RenateODManager(beamlet_geometry, shot_number, time, species, energy)
     radial_coordinate, relative_attenuation = r.get_attenuation_profile()
         
-    export_directory = export_root + '/' + shot_number + '_' + time + '/' + species + '/' + energy + '/'
+    export_directory = os.path.join(export_root, shot_number + '_' + time, species, energy)
     try:
-        os.mkdir(export_directory)
+        os.makedirs(export_directory)
         print('Create directory and write data to ' + export_directory)
-    except FileExistsError:
-        print('Write data to ' + export_directory)
-    else:
-        pass
+    except OSError as error:
+        print('Cannot create and write to ' + export_directory)
     print('Save RENATE-OD ionisation profile to: ' + export_directory)
-    radial_coordinate.fillna(0).to_csv(export_directory + 'rad.dat', index=False, header=False)
-    relative_attenuation.fillna(0).to_csv(export_directory + 'ionyeald.dat', index=False, header=False)
+    radial_coordinate.fillna(0).to_csv(os.path.join(export_directory, 'rad.dat'), index=False, header=False)
+    relative_attenuation.fillna(0).to_csv(os.path.join(export_directory, 'ionyeald.dat'), index=False, header=False)
     plot_attenuation_profile(shot_number, time, radial_coordinate, relative_attenuation, export_directory)
 
 
 def get_lcfs_radial_coordinate(shot_number, time, efit_reconstruction_id=1, database_directory='input/cdb', efit_subdir='EFITXX'):
-    data_directory = get_home_directory() + '/' + database_directory + '/' + str(shot_number)
-    efit_file = data_directory + '/' + efit_subdir + '/' + 'EFITXX.' + str(efit_reconstruction_id) + '.h5'
+    data_directory = os.path.join(get_home_directory(), database_directory, str(shot_number))
+    efit_file = os.path.join(data_directory, efit_subdir, 'EFITXX.' + str(efit_reconstruction_id) + '.h5')
     efit = EFITManager(efit_file, time)
     return efit.get_time_sliced_data('output/separatrixGeometry/rmidplaneOut')
 
@@ -47,14 +45,14 @@ def plot_attenuation_profile(shot_number, time, radial_coordinate, relative_atte
     R_LCFS = get_lcfs_radial_coordinate(shot_number, time)
     matplotlib.pyplot.axvline(R_LCFS, c='red', ls='--')
     matplotlib.pyplot.text(R_LCFS+0.005, 0.45, 'LCFS', c='red', fontsize=12)
-    matplotlib.pyplot.savefig(export_directory+'/attenuation.pdf')
-    matplotlib.pyplot.savefig(export_directory+'/attenuation.svg')
-    print('Attenuation profile saved as: '+export_directory+'/attenuation.pdf')
+    matplotlib.pyplot.savefig(os.path.join(export_directory, 'attenuation.pdf'))
+    matplotlib.pyplot.savefig(os.path.join(export_directory, 'attenuation.svg'))
+    print('Attenuation profile saved to '+ export_directory)
     matplotlib.pyplot.show()
 
 
 if __name__ == "__main__":
     a_shot_number = '17178'
     a_time = '1097'
-    a_species='Li'
+    a_species = 'Li'
     export_beamlet_profile(shot_number=a_shot_number, time=a_time, species=a_species)
